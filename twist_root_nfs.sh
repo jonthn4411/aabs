ead301cb09f5901f99ab81dbe9b78058ac3bb9f7
Johnny Xia <bxia1@marvell.com>
2009-05-12 15:29:10 +0800

    twist init.rc to simulate sdcard mounted event automatically
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

--- twist_root_nfs.sh
+++ twist_root_nfs.sh
@@ -4,20 +4,57 @@
 # This script can be run multiple times on the root nfs folder and still
 # generate the proper result.
 
+function gen_init_sdcard_sh()
+{
+	cat > $1 <<-EOF
+	#!/system/bin/sh
+
+	chmod 0777 /sdcard
+	chmod 0444 /sdcard/*.*
+	setprop EXTERNAL_STORAGE_STATE mounted
+	
+	#touch the directory. A trick for NFS
+	ls /sdcard >/dev/null	
+	sleep 2s
+	am broadcast -a android.intent.action.MEDIA_MOUNTED --ez read-only false -d file:///sdcard
+	EOF
+}
+
 echo "  modifying init.rc..." &&
-sed -in "/^[ tab]*mount yaffs2/ s/mount/#(for nfs)mount/" init.rc &&
+sed -i "/^[ tab]*mount yaffs2/ s/mount/#(for nfs)mount/" init.rc &&
 
-sed -in "/^[ tab]*mount rootfs rootfs/ s/mount/#(for nfs)mount/" init.rc &&
+sed -i "/^[ tab]*mount rootfs rootfs/ s/mount/#(for nfs)mount/" init.rc &&
 
-sed -in '/^[ tab]*mkdir \/sdcard 0000 system system/ {
+sed -i '/^[ tab]*mkdir \/sdcard 0000 system system/ {
 s/mkdir/#(for nfs)mkdir/ 
 a\
+\
     #>>>for nfs\
-    mkdir \/sdcard 0777 root root\
-    setprop EXTERNAL_STORAGE_STATE mounted\
+    mkdir \/sdcard 0777 system system\
+    chmod 0777 \/sdcard\
     export amsd \"am broadcast -a android.intent.action.MEDIA_MOUNTED --ez read-only false -d file:\/\/\/sdcard\"\
-    #<<<for nfs
+    #<<<for nfs\
+
 }' init.rc &&
+
+if grep "service init-sdcard /init.sdcard.sh" init.rc; then
+  echo "  already have service init-sdcard define..."
+else
+  cat >>init.rc <<-EOF
+	
+	#for nfs
+	service init-sdcard /init.sdcard.sh
+	    user root
+	    group root
+	    oneshot
+	#for nfs
+	
+	EOF
+fi &&
+
+gen_init_sdcard_sh init.sdcard.sh &&
+
+chmod 0755 init.sdcard.sh &&
 
 echo "  chmod a+r system/usr/keychars/*..." &&
 chmod a+r ./system/usr/keychars/* 
