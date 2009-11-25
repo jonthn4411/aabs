18cade3d9f6103aae56783e0db1132879487554b
Johnny Xia <bxia1@marvell.com>
2009-11-25 14:39:53 +0800

    avlite:update for including obm for 1.6F board
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

--- avlite/build-obm.mk
+++ avlite/build-obm.mk
@@ -1,9 +1,15 @@
 #check if the required variables have been set.
 $(call check-variables,OBM_SRC_DIR BUILD_VARIANTS)
 
-OBM_NTIM_STEM:=NTIM_OBM_uboot_ASPEN_Mode
-OBM_NTLOADER:=ASPN_NTLOADER.bin
+OBM_NTIM_1:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.bin
+OBM_NTIM_2:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.bin
+OBM_DESC_1:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.txt
+OBM_DESC_2:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.txt
 
+OBM_NTLOADER_1:=ASPN_NTLOADER_avengers-a_slc.bin
+OBM_NTLOADER_2:=ASPN_NTLOADER_spi.bin
+
+OBM_BIN_DIR:=binaries/BootLoader
 #$1:build variant
 define define-build-obm
 
@@ -11,19 +17,23 @@ define define-build-obm
 #m:means mandatory
 #o:means optional
 #md5: need to generate md5 sum
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_STEM)2.bin:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_STEM)3.bin:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_STEM)5.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_2):m:md5
 
 .PHONY:build_obm_$(1)
 build_obm_$(1):
 	$$(log) "start to copy obm files"
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/binaries/avengers/$$(OBM_NTLOADER) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/binaries/avengers/$$(OBM_NTIM_STEM)2.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/binaries/avengers/$$(OBM_NTIM_STEM)3.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/binaries/avengers/$$(OBM_NTIM_STEM)5.bin $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_NTLOADER_2) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(OBM_SRC_DIR)/$$(OBM_BIN_DIR)/$$(OBM_DESC_2) $$(OUTPUT_DIR)/$(1)
 	$$(log) "  done."
 endef
 
