9ff42e2ab4f55fc6e174eeb4c6b028cf13843e5d
Johnny Xia <bxia1@marvell.com>
2009-11-25 17:47:55 +0800

    avlite:add avlite-eclair build script
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

---
+++ avlite-eclair.mk
@@ -0,0 +1,6 @@
+MANIFEST_BRANCH:=avlite-eclair
+BOARD:=avlite
+ANDROID_VERSION:=eclair
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+include avlite/board.mk
---
+++ build-avlite-eclair.sh
@@ -0,0 +1,5 @@
+export ABS_BOARD=avlite
+export ABS_DROID_BRANCH=eclair
+export ABS_PRODUCT_NAME=Avengers
+
+core/autobuild.sh $*
