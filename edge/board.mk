4c1fd5b0c44e03f899902b8c46a737009d0f1dc7
Johnny Xia <bxia1@marvell.com>
2009-12-25 13:08:07 +0800

    edge:change uboot's configuration to edge_config
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

--- edge/board.mk
+++ edge/board.mk
@@ -27,7 +27,7 @@ include $(BOARD)/build-droid-kernel.mk
 #
 # Include goal for build UBoot
 #
-UBOOT_CONFIG:=avengers_config
+UBOOT_CONFIG:=edge_config
 UBOOT_SRC_DIR:=boot/uboot
 include $(BOARD)/build-uboot.mk
 
