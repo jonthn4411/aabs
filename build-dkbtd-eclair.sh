4b089c817a9438dcb307206f3d9f9359b50663c1
magicyu <magicyu@magicyu-desktop.(none)>
2010-01-29 18:43:30 +0800

    DKBTD: align dkbtd autobuild to johnny's architecture             dkbtd autobuild added

---
+++ build-dkbtd-eclair.sh
@@ -0,0 +1,5 @@
+export ABS_BOARD=dkbtd
+export ABS_DROID_BRANCH=eclair
+export ABS_PRODUCT_NAME=dkbtd
+
+core/autobuild.sh $*
---
+++ dkbtd-eclair.mk
@@ -0,0 +1,6 @@
+MANIFEST_BRANCH:=dkbttc-eclair
+BOARD:=dkbtd
+ANDROID_VERSION:=eclair
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+include dkbtd/board.mk
