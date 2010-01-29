f482eb59ddcc4258bb7120d8c3252af630263cb0
TonyTeng <ylteng@marvell.com>
2010-01-29 19:04:51 +0800

    evbpv2:init aabs
    Signed-off-by: TonyTeng <ylteng@marvell.com>

---
+++ build-evbpv2-eclair.sh
@@ -0,0 +1,5 @@
+export ABS_BOARD=evbpv2
+export ABS_DROID_BRANCH=eclair
+export ABS_PRODUCT_NAME=evbpv2
+
+core/autobuild.sh $*
---
+++ evbpv2-eclair.mk
@@ -0,0 +1,6 @@
+MANIFEST_BRANCH:=evbpv2-eclair
+BOARD:=evbpv2
+ANDROID_VERSION:=eclair
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+include evbpv2/board.mk
