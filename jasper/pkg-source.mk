ecc517cfaa04487efc4cf193afddc3052580b059
Hurri Lu <jlu32@marvell.com>
2010-01-29 17:30:13 +0800

    jasper : change for new build scripts

--- jasper/pkg-source.mk
+++ jasper/pkg-source.mk
@@ -1,8 +1,42 @@
 include core/pkg-source.mk
 
+INTERNAL_PROJECTS :=vendor/marvell/external/helix
+INTERNAL_PROJECTS +=vendor/marvell/external/flash
+INTERNAL_PROJECTS +=vendor/marvell/generic/apps/CmmbPlayer
+INTERNAL_PROJECTS +=vendor/marvell/generic/apps/CmmbStack
+
+EXCLUDE_VCS=--exclude-vcs --exclude=.repo
+
+KERNEL_BASE_COMMIT:=8e0ee43bc2c3e19db56a4adaa9a9b04ce885cd84
+
+ifeq ($(ANDROID_VERSION),donut)
+	DROID_BASE:=shgit/donut-release
+else
+	DROID_BASE:=shgit/$(ANDROID_VERSION)
+endif
+
 .PHONY:pkgsrc
 pkgsrc: output_dir get_source_for_pkg
-	$(hide)$(TOP_DIR)/$(BOARD)/pkgsrc.sh $(OUTPUT_DIR)
+	$(hide)echo "  save project list"
+	$(hide)cd $(OUTPUT_DIR)/source && repo forall -c "echo -n \$$(pwd):;echo \$$REPO_PROJECT" > $(OUTPUT_DIR)/prjlist
+
+	$(hide)echo "  remove internal source code..."
+	$(hide)cd $(OUTPUT_DIR)/source && for prj in $(INTERNAL_PROJECTS); do rm -fr $$prj; done
+
+	$(hide)echo "  package all source code..."
+	$(hide)cd $(OUTPUT_DIR) && tar czf droid_all_src.tgz $(EXCLUDE_VCS) source/
+
+	$(hide)echo "  package kernel source code..."
+	$(hide)cd $(OUTPUT_DIR) && $(TOP_DIR)/core/gen_kernel_src_patch.sh $(KERNEL_BASE_COMMIT)
+
+#	$(hide)echo "  package uboot obm source code..."
+#	$(hide)cd $(OUTPUT_DIR) && $(TOP_DIR)/core/gen_uboot_obm_src_patch.sh $(UBOOT_BASE_COMMIT)
+
+	$(hide)echo "  package android source code..."
+	$(hide)cd $(OUTPUT_DIR) && $(TOP_DIR)/core/gen_droid_src_patch.sh $(DROID_BASE) $(TOP_DIR)/core
+
+	$(hide)cp $(TOP_DIR)/core/setup_android.sh $(OUTPUT_DIR)
+	$(hide)cp $(BOARD)/ReleaseNotes.txt $(OUTPUT_DIR)
 	$(log) "  done."
 
 
@@ -10,8 +44,15 @@ pkgsrc: output_dir get_source_for_pkg
 #m:means mandatory
 #o:means optional
 #md5: need to generate md5 sum
-PUBLISHING_FILES+=kernel_src.tgz:o:md5 \
-	droid_src.tgz:o:md5 \
-	drivers_src.tgz:o:md5\
-	boot_src.tgz:o:md5
+PUBLISHING_FILES+=droid_all_src.tgz:o:md5 
+PUBLISHING_FILES+=android_src.tgz:m:md5 
+PUBLISHING_FILES+=android_patches.tgz:m:md5 
+PUBLISHING_FILES+=kernel_src.tgz:m:md5 
+PUBLISHING_FILES+=kernel_patches.tgz:m:md5 
+#PUBLISHING_FILES+=uboot_src.tgz:m:md5 
+#PUBLISHING_FILES+=uboot_patches.tgz:m:md5 
+#PUBLISHING_FILES+=obm_src.tgz:m:md5 
+PUBLISHING_FILES+=marvell_manifest.xml:m
+PUBLISHING_FILES+=setup_android.sh:m
+PUBLISHING_FILES+=ReleaseNotes.txt:m
 
