cd9fd866cb1799acafe4c77450ddf4bab48a6d42
Johnny Xia <bxia1@marvell.com>
2010-02-02 16:30:21 +0800

    core: modify pkg-source to link the .repo folder to reduce download time
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

--- core/pkg-source.mk
+++ core/pkg-source.mk
@@ -4,15 +4,21 @@ get_source_for_pkg: output_dir
 	$(log) "Package source code using the $(OUTPUT_DIR)/manifest.xml"
 	#check if $(OUTPUT_DIR)/manifest.xml is generated already
 	$(hide)[ -s $(OUTPUT_DIR)/manifest.xml ]
-	$(hide)if [ ! -d "$(OUTPUT_DIR)/source" ]; then \
-		mkdir $(OUTPUT_DIR)/source; \
+
+	$(hide)if [ -d "$(OUTPUT_DIR)/source" ]; then \
+		rm -fr $(OUTPUT_DIR)/source; \
 	fi
+
+	$(hide)mkdir -p $(OUTPUT_DIR)/source
+
+	$(hide)cd $(OUTPUT_DIR)/source/ && \
+	ln -s $(SRC_DIR)/.repo .repo
+
 	$(log) " getting source code using manifest.xml"
 	$(hide)cd $(OUTPUT_DIR)/source && \
-	repo init -u ssh://$(GIT_MANIFEST) -b $(MANIFEST_BRANCH) --repo-url ssh://$(GIT_REPO) && \
 	cp $(OUTPUT_DIR)/manifest.xml $(OUTPUT_DIR)/source/.repo/manifests/autobuild.xml && \
 	repo init -m autobuild.xml && \
-	repo sync 
+	repo sync -l 
 
 
 
