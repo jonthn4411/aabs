18c5422c516a41705877642e43202d74d0f7a09e
TonyTeng <ylteng@marvell.com>
2010-02-09 19:03:43 +0800

    evb3mg1: init aabs
    Signed-off-by: TonyTeng <ylteng@marvell.com>

---
+++ build-evb3mg1-eclair.sh
@@ -0,0 +1,5 @@
+export ABS_BOARD=evb3mg1
+export ABS_DROID_BRANCH=eclair
+export ABS_PRODUCT_NAME=evb3mg1
+
+core/autobuild.sh $*
---
+++ evb3mg1-eclair.mk
@@ -0,0 +1,6 @@
+MANIFEST_BRANCH:=evb3mg1-eclair
+BOARD:=evb3mg1
+ANDROID_VERSION:=eclair
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+include evb3mg1/board.mk
