3a78e9e4a721ff2e77d1e5fe9bef569cddae130b
jianchun <jzuo@jzuo-ubuntu.(none)>
2010-03-09 17:58:47 +0800

    avlite: swith OBM from SLC NAND to SPI NOR
    Signed-off-by: jianchun <jzuo@jzuo-ubuntu.(none)>

--- avlite/build-uboot-obm.mk
+++ avlite/build-uboot-obm.mk
@@ -3,14 +3,14 @@ $(call check-variables,BUILD_VARIANTS)
 BOOT_SRC_DIR:=boot
 BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
 
-OBM_NTIM_1:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.bin
-#OBM_NTIM_2:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.bin
+#OBM_NTIM_1:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.bin
+OBM_NTIM_2:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.bin
 
-OBM_DESC_1:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.txt
-#OBM_DESC_2:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.txt
+#OBM_DESC_1:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_slc.txt
+OBM_DESC_2:=desc_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.txt
 
-OBM_NTLOADER_1:=ASPN_NTLOADER_avengers-a_slc.bin
-#OBM_NTLOADER_2:=ASPN_NTLOADER_spi.bin
+#OBM_NTLOADER_1:=ASPN_NTLOADER_avengers-a_slc.bin
+OBM_NTLOADER_2:=ASPN_NTLOADER_spi.bin
 
 
 #$1:build variant
@@ -20,12 +20,12 @@ define define-build-uboot-obm
 #o:means optional
 #md5: need to generate md5 sum
 PUBLISHING_FILES+=$(1)/u-boot.bin:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_1):m:md5
-#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_2):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_1):m:md5
-#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_2):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_1):m:md5
-#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_2):m:md5
+#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_2):m:md5
+#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_2):m:md5
+#PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_2):m:md5
 
 
 .PHONY:build_uboot_obm_$(1)
@@ -37,12 +37,12 @@ build_uboot_obm_$(1):
 
 	$$(log) "start to copy uboot and obm files"
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)
-#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTLOADER_2) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)
-#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)
-#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_DESC_2) $$(OUTPUT_DIR)/$(1)
+#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTLOADER_2) $$(OUTPUT_DIR)/$(1)
+#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)
+#	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_DESC_2) $$(OUTPUT_DIR)/$(1)
 	$$(log) "  done."
 
 endef
