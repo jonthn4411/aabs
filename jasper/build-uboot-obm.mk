fdb6f145e9ab3e49e48b8602d6549088eeb7af1f
Hurri Lu <jlu32@marvell.com>
2010-04-26 13:19:58 +0800

    jasper : build both nand and emmc obm for bonnell board
    Signed-off-by: Hurri Lu <jlu32@marvell.com>

--- jasper/build-uboot-obm.mk
+++ jasper/build-uboot-obm.mk
@@ -6,10 +6,13 @@ UBOOT:=u-boot.bin
 OBM_NTIM_1:=ntim_mmp2_nand_bbu_ddr.bin
 OBM_DESC_1:=ntim_mmp2_emmc_ddr_elipda_512m.txt
 OBM_NTLOADER_1:=MMP2_NTLOADER_3_2_15.bin
-WTM:=WtmUnresetPJ4.bin
+WTM_1:=WtmUnresetPJ4.bin
 PARTITION_BIN:=partition.bin
 PARTITION_DESC:=partition.txt
-
+OBM_NTIM_2:=ntim_mmp2_nand_bbu_ddr.bin
+OBM_DESC_2:=ntim_mmp2_nand_bbu_ddr_elipda_512m.txt
+OBM_NTLOADER_2:=MMP2_NTLOADER_3_2_15.bin
+WTM_2:=WtmUnresetPJ4.bin
 
 #$1:build variant
 define define-build-uboot-obm
@@ -18,12 +21,17 @@ define define-build-uboot-obm
 #o:means optional
 #md5: need to generate md5 sum
 PUBLISHING_FILES+=$(1)/$(UBOOT):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTLOADER_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_NTIM_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(OBM_DESC_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(WTM):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(PARTITION_BIN):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(PARTITION_DESC):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTLOADER_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(WTM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(PARTITION_BIN):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(PARTITION_DESC):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_NTLOADER_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_NTIM_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_DESC_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nand/$(WTM_2):m:md5
+
 
 
 .PHONY:build_uboot_obm_$(1)
@@ -32,15 +40,21 @@ build_uboot_obm_$(1):
 	$$(hide)cd $$(SRC_DIR)/$$(BOOT_SRC_DIR) && \
 	make all
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/nand
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc
 
 	$$(log) "start to copy uboot and obm files"
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(WTM) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(PARTITION_BIN) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(PARTITION_DESC) $$(OUTPUT_DIR)/$(1) 
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(WTM_1) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(PARTITION_BIN) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(PARTITION_DESC) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_NTLOADER_2) $$(OUTPUT_DIR)/$(1)/nand
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)/nand
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_DESC_2) $$(OUTPUT_DIR)/$(1)/nand
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(WTM_2) $$(OUTPUT_DIR)/$(1)/nand
 	$$(log) "  done."
 
 endef
