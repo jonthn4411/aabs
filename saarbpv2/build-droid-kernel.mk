f2719c03b3ba40ec1820507153c1aea5ea255140
TonyTeng <ylteng@marvell.com>
2010-05-09 23:24:07 -0400

    saarbpv2: update build-droid-kernel.mk for pxafs.img publish
    Signed-off-by: TonyTeng <ylteng@marvell.com>

--- saarbpv2/build-droid-kernel.mk
+++ saarbpv2/build-droid-kernel.mk
@@ -155,7 +155,7 @@ package_droid_mmc_$(1)_$(2):
 	$$(hide)cp -r -p $$(OUTPUT_DIR)/$(2)/root $$(OUTPUT_DIR)/$(2)/root_nfs && \
 	cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system $$(OUTPUT_DIR)/$(2)/root_nfs && \
 	cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/data $$(OUTPUT_DIR)/$(2)/root_nfs
-	if [ "$$(TARGET_PRODUCT)" = "ttc_jil" ] || [ "$$(TARGET_PRODUCT)" = "td_jil" ] || [ "$$(TARGET_PRODUCT)" = "dkbttc" ] || [ "$$(TARGET_PRODUCT)" = "td_dkb" ]; then cp -rfp $$(SRC_DIR)/vendor/marvell/generic/ttc_telephony/drivers/output/marvell $$(OUTPUT_DIR)/$(2)/root_nfs; fi
+	cp -rfp $$(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/output/marvell $$(OUTPUT_DIR)/$(2)/root_nfs
 	$$(log) "  updating the modules..."
 	$$(hide)if [ -d $$(OUTPUT_DIR)/$(2)/modules ]; then rm -fr $$(OUTPUT_DIR)/$(2)/modules; fi
 	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar xzf modules_android_mmc.tgz && cp -r modules $$(OUTPUT_DIR)/$(2)/root_nfs/system/lib/
@@ -223,11 +223,7 @@ root:=$$(word 4, $$(tw) )
 #PUBLISHING_FILES_$(2):=$(PUBLISHING_FILES_$(2)) $(2)/zImage.$$(os).$$(storage):m:md5
 PUBLISHING_FILES_$(2)+=$(2)/zImage.$$(os).$$(storage):m:md5
 #PUBLISHING_FILES_$(2)+=$(2)/modules_$$(os)_$$(storage).tgz:m:md5
-
-ifneq ($(filter $(ABS_PRODUCT_NAME),td_jil td_dkb ttc_jil ttc_dkb dkbttc),)
-PUBLISHING_FILES_$(2)+=$(2)/pxafs.img:m:md5
-PUBLISHING_FILES_$(2)+=$(2)/Boerne_DIAG.mdb.txt:m:md5
-endif
+PUBLISHING_FILES_$(2)+=$(2)/pxafs_ext2.img:m:md5
 
 build_kernel_$$(os)_$$(storage)_$(2): private_os:=$$(os)
 build_kernel_$$(os)_$$(storage)_$(2): private_storage:=$$(storage)
@@ -247,8 +243,7 @@ build_kernel_$$(os)_$$(storage)_$(2): output_dir $$(if $$(findstring root,$$(roo
 	mkdir -p $$(OUTPUT_DIR)/$(2)/modules
 	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/modules/* $$(OUTPUT_DIR)/$(2)/modules
 	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar czf modules_$$(private_os)_$$(private_storage).tgz modules/ 
-	$$(hide)if [ "$$(TARGET_PRODUCT)" = "dkbttc" ] || [ "$$(TARGET_PRODUCT)" = "dkbtd" ]; then \
-	cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/telephony/* /$$(OUTPUT_DIR)/$(2); fi
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/telephony/* /$$(OUTPUT_DIR)/$(2)/
 	$(log) "  done."
 
 .PHONY: build_kernel_$$(os)_$$(storage)_$(2)
