bffa36f867281b41aa08fddfbe6062dea58255c5
Magic Yu <magicyu@marvell.com>
2010-05-12 11:11:13 +0800

    dkb: add publish Software_Downloader.zip

--- dkbttc/build-uboot-obm.mk
+++ dkbttc/build-uboot-obm.mk
@@ -17,11 +17,12 @@ PUBLISHING_FILES_$(1)+=$(1)/ReliableData.bin:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/Arbel_DIAG.mdb:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/Arbel_NVM.mdb:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/ramdisk_no_gui.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/Software_Downloader.zip:m:md5
 
 .PHONY:build_uboot_obm_$(1)
 build_uboot_obm_$(1):
 	$$(hide)cd $$(SRC_DIR)/boot && \
-	make uboot
+	make all
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
 	$$(log) "start to copy obm and CP image files"
 	$$(hide)cp $$(SRC_DIR)/boot/out/* $$(OUTPUT_DIR)/$(1)
@@ -31,6 +32,7 @@ build_uboot_obm_$(1):
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/TTC1_M05_A0_AI_Flash.bin $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/ReliableData.bin $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/ramdisk_no_gui.img $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/Software_Downloader.zip $$(OUTPUT_DIR)/$(1)
 	$$(log) "cp OBM and CP images  done."
 
 endef
