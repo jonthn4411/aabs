7d6025706d041ea4867fb8b2b3e87d53b8f16549
Weichuan Yan <wchyan@marvell.com>
2010-06-10 16:27:59 +0800

    saarbpv2: add the software download package
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- saarbpv2/board.mk
+++ saarbpv2/board.mk
@@ -27,14 +27,17 @@ include $(BOARD)/build-droid-kernel.mk
 # Include goal for build UBoot and OBM
 include $(BOARD)/build-uboot-obm.mk
 
+# Include goal for build software downloader
+include $(BOARD)/build-swd.mk
+
 #define the combined goal to include all build goals
 define define-build
-build_$(1): build_droid_kernel_$(1) build_uboot_obm_$(1)
+build_$(1): build_droid_kernel_$(1) build_uboot_obm_$(1) build_swd_$(1)
 build_uboot_obm_$(1): build_droid_kernel_$(1)
 endef
 $(foreach bv, $(BUILD_VARIANTS), $(eval $(call define-build,$(bv) ) ) )
 
-clean:clean_droid_kernel clean_uboot_obm
+clean:clean_droid_kernel clean_uboot_obm clean_swd
 
 #
 # Include publish goal
