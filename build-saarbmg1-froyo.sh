0666748de18895c2f1792be4e11b4f9569e8fa46
Gavin Gu <gavin.gu@marvell.com>
2010-07-01 12:05:17 +0800

    saarbmg1:enable saarbmg1-froyo aabs
    Signed-off-by: Gavin Gu <gavin.gu@marvell.com>

---
+++ build-saarbmg1-froyo.sh
@@ -0,0 +1,5 @@
+export ABS_BOARD=saarbmg1
+export ABS_DROID_BRANCH=froyo
+export ABS_PRODUCT_NAME=saarbmg1
+
+core/autobuild.sh $*
---
+++ saarbmg1-froyo.mk
@@ -0,0 +1,6 @@
+MANIFEST_BRANCH:=saarbmg1-froyo
+BOARD:=saarbmg1
+ANDROID_VERSION:=froyo
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+include saarbmg1/board.mk
