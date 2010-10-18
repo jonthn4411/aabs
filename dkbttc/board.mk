6403c5c95aa658ac6f98d9845b10ea5280354436
xqian <xqian@marvell.com>
2010-10-18 16:23:54 +0800

    dkbttc: adjust kernel/uboot build order
    Signed-off-by: xqian <xqian@marvell.com>

--- dkbttc/board.mk
+++ dkbttc/board.mk
@@ -29,7 +29,7 @@ include $(BOARD)/build-uboot-obm.mk
 
 #define the combined goal to include all build goals
 define define-build
-build_$(1): build_droid_kernel_$(1) build_uboot_obm_$(1)
+build_$(1): build_uboot_obm_$(1) build_droid_kernel_$(1)
 endef
 $(foreach bv, $(BUILD_VARIANTS), $(eval $(call define-build,$(bv) ) ) )
 
