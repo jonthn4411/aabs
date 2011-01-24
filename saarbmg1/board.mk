434875581d33010ccd1cc01a8685ad618a6b3082
Weichuan Yan <wchyan@marvell.com>
2011-01-24 16:28:24 +0800

    saarbmg1: add wtpsp auto build
    Change-Id: Ic2e822f70d4f85e09449d3eef6c41abdd2df63c2
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- saarbmg1/board.mk
+++ saarbmg1/board.mk
@@ -30,14 +30,18 @@ include $(BOARD)/build-uboot-obm.mk
 # Include goal for build software downloader
 include $(BOARD)/build-swd.mk
 
+# Include goal for build wtpsp
+include $(BOARD)/build-wtpsp.mk
+
 #define the combined goal to include all build goals
 define define-build
-build_$(1): build_droid_kernel_$(1) build_uboot_obm_$(1) build_swd_$(1)
+build_$(1): build_droid_kernel_$(1) build_uboot_obm_$(1) build_swd_$(1) build_wtpsp_$(1)
 build_uboot_obm_$(1): build_droid_kernel_$(1)
+build_wtpsp_$(1): build_droid_kernel_$(1)
 endef
 $(foreach bv, $(BUILD_VARIANTS), $(eval $(call define-build,$(bv) ) ) )
 
-clean:clean_droid_kernel clean_uboot_obm clean_swd
+clean:clean_droid_kernel clean_uboot_obm clean_swd clean_wtpsp
 
 #
 # Include publish goal
