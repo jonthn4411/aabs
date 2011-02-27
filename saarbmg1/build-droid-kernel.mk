66152a49594cf334654f9ac17610bba032c82d65
Weichuan Yan <wchyan@marvell.com>
2011-02-27 14:02:47 +0800

    saarbmg1: publish the build.prop
    Change-Id: I04f72c8fbcda563d3c5c90470da23649b6eb821a
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- saarbmg1/build-droid-kernel.mk
+++ saarbmg1/build-droid-kernel.mk
@@ -66,6 +66,7 @@ PUBLISHING_FILES_$(1)+=$(1)/userdata_onenand.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/system_onenand.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/ramdisk.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/symbols_lib.tgz:o:md5
+PUBLISHING_FILES_$(1)+=$(1)/build.prop:o:md5
 endef
 
 
@@ -142,7 +143,8 @@ package_droid_slc_$(1)_$(2):
 	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_ext3.img $$(OUTPUT_DIR)/$(2)/system_ext3.img && \
 	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_ext3.img $$(OUTPUT_DIR)/$(2)/userdata_ext3.img && \
 	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_onenand.img $$(OUTPUT_DIR)/$(2)/system_onenand.img && \
-	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_onenand.img $$(OUTPUT_DIR)/$(2)/userdata_onenand.img
+	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_onenand.img $$(OUTPUT_DIR)/$(2)/userdata_onenand.img && \
+	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system/build.prop $$(OUTPUT_DIR)/$(2)/build.prop
 	$$(hide)cp -a $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/symbols/system/lib $$(OUTPUT_DIR)/$(2)/
 	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar czf symbols_lib.tgz lib && rm lib -rf
 	$$(log) "  done for package_droid_slc_$(1)_$(2)."
