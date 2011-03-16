354404dfd0324f3d5871b6685056785be99ac443
Horace Hsieh <xiehong@marvell.com>
2011-03-16 19:51:49 +0800

    brownstone: remove update-bootloader.zip
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- brownstone/build-droid-kernel.mk
+++ brownstone/build-droid-kernel.mk
@@ -63,7 +63,6 @@ build_droid_root_$(1): output_dir
 		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system.img $$(OUTPUT_DIR)/$(1)/
 	echo "    copy update packages..." && \
 		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/update_droid.zip $$(OUTPUT_DIR)/$(1)/ && \
-		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/update_bootloader.zip $$(OUTPUT_DIR)/$(1)/
 	echo "    generating symbols_lib.tgz..." && \
 		cp -a $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/symbols/system/lib $$(OUTPUT_DIR)/$(1)/ && \
 		cd $$(OUTPUT_DIR)/$(1) && tar czf symbols_lib.tgz lib && rm lib -rf
@@ -75,7 +74,6 @@ PUBLISHING_FILES_$(1)+=$(1)/ramdisk.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/ramdisk_recovery.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/system.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/update_droid.zip:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/update_bootloader.zip:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/symbols_lib.tgz:o:md5
 endef
 
