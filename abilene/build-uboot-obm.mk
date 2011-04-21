42c180e83b3ecb4c06d1ee29336a5ee4410871e8
Pengcheng Guo <pcguo@marvell.com>
2011-04-21 18:47:48 +0800

    abilene: modify uboot and obm makefile for abilene build
    Change-Id: I5dbdcf6cce2121a378dc2ecf8a35d15f18b3494c
    Signed-off-by: Pengcheng Guo <pcguo@marvell.com>

--- abilene/build-uboot-obm.mk
+++ abilene/build-uboot-obm.mk
@@ -3,16 +3,10 @@ $(call check-variables,BUILD_VARIANTS)
 BOOT_SRC_DIR:=boot
 BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
 UBOOT:=u-boot.bin
-OBM_NTIM_1:=ntim_mmp2_nand_bbu_ddr.bin
-OBM_DESC_1:=ntim_mmp2_emmc_ddr3_elipda_1g.txt
-OBM_NTLOADER_1:=MMP2_NTLOADER_3_2_17.bin
-WTM_1:=Wtm_rel_mmp2.bin
-PARTITION_BIN:=partition.bin
-PARTITION_DESC:=partition.txt
-OBM_NTIM_2:=ntim_mmp2_nand_bbu_ddr.bin
-OBM_DESC_2:=ntim_mmp2_nand_ddr3_elipda_1g.txt
-OBM_NTLOADER_2:=MMP2_NTLOADER_3_2_17.bin
-WTM_2:=Wtm_rel_mmp2.bin
+OBM_NTIM_1:=ntim_mmp3_v7_mp_emmc_dis_uboot.bin
+OBM_DESC_1:=ntim_mmp3_v7_mp_emmc_dis_uboot.txt
+LOOP_BIN:=LoopSelf.bin
+WTM_1:=wtm_400_MHz.bin
 
 #$1:build variant
 define define-build-uboot-obm
@@ -21,16 +15,10 @@ define define-build-uboot-obm
 #o:means optional
 #md5: need to generate md5 sum
 PUBLISHING_FILES+=$(1)/$(UBOOT):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTLOADER_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTIM_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_DESC_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(WTM_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/emmc/$(PARTITION_BIN):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/emmc/$(PARTITION_DESC):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_NTLOADER_2):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_NTIM_2):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/nand/$(OBM_DESC_2):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/nand/$(WTM_2):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(LOOP_BIN):m:md5
 
 
 
@@ -40,21 +28,14 @@ build_uboot_obm_$(1):
 	$$(hide)cd $$(SRC_DIR)/$$(BOOT_SRC_DIR) && \
 	make all
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
-	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/nand
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc
 
 	$$(log) "start to copy uboot and obm files"
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(WTM_1) $$(OUTPUT_DIR)/$(1)/emmc
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(PARTITION_BIN) $$(OUTPUT_DIR)/$(1)/emmc
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(PARTITION_DESC) $$(OUTPUT_DIR)/$(1)/emmc
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_NTLOADER_2) $$(OUTPUT_DIR)/$(1)/nand
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)/nand
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(OBM_DESC_2) $$(OUTPUT_DIR)/$(1)/nand
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nand/$$(WTM_2) $$(OUTPUT_DIR)/$(1)/nand
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(LOOP_BIN) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(log) "  done."
 
 endef
