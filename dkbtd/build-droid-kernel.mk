25d218093bca86a4a3f775cbf536a6fde2a4bbae
yyli <yyli@marvell.com>
2011-04-22 09:26:27 +0800

    Change eMMC images format to EXT4
    Signed-off-by: yyli <yyli@marvell.com>

--- dkbtd/build-droid-kernel.mk
+++ dkbtd/build-droid-kernel.mk
@@ -59,16 +59,16 @@ build_droid_root_$(1): output_dir
 	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/root $$(OUTPUT_DIR)/$(1) 
 	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/ramdisk.img $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata.img $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_ext3.img $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_ext4.img $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system.img $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_ext3.img $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_ext4.img $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/ramdisk-recovery.img $$(OUTPUT_DIR)/$(1)
 	$(log) "  done"
 
 PUBLISHING_FILES_$(1)+=$(1)/userdata.img:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/userdata_ext3.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/userdata_ext4.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/system.img:m:md5
-PUBLISHING_FILES_$(1)+=$(1)/system_ext3.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/system_ext4.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/ramdisk.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/ramdisk-recovery.img:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/symbols_lib.tgz:o:md5
@@ -147,8 +147,8 @@ package_droid_slc_$(1)_$(2):
 	echo "    copy  image files..." && \
 	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system.img $$(OUTPUT_DIR)/$(2)/system.img && \
 	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata.img $$(OUTPUT_DIR)/$(2)/userdata.img &&\
-	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_ext3.img $$(OUTPUT_DIR)/$(2)/ && \
-	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_ext3.img $$(OUTPUT_DIR)/$(2)/
+	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system_ext4.img $$(OUTPUT_DIR)/$(2)/ && \
+	cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata_ext4.img $$(OUTPUT_DIR)/$(2)/
 	$$(hide)cp -a $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/symbols/system/lib $$(OUTPUT_DIR)/$(2)/
 	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar czf symbols_lib.tgz lib && rm lib -rf
 	$$(log) "  done for package_droid_slc_$(1)$(2)."
@@ -229,7 +229,7 @@ PUBLISHING_FILES_$(2)+=$(2)/$(KERNEL_IMAGE).$$(os).$$(storage):m:md5
 PUBLISHING_FILES_$(2)+=$(2)/modules_$$(os)_$$(storage).tgz:m:md5
 
 PUBLISHING_FILES_$(2)+=$(2)/pxafs.img:m:md5
-PUBLISHING_FILES_$(2)+=$(2)/pxafs_ext3.img:m:md5
+PUBLISHING_FILES_$(2)+=$(2)/pxafs_ext4.img:m:md5
 PUBLISHING_FILES_$(2)+=$(2)/Boerne_DIAG.mdb.txt:m:md5
 PUBLISHING_FILES_$(2)+=$(2)/vmlinux:m:md5
 PUBLISHING_FILES_$(2)+=$(2)/System.map:m:md5
