9f0e6e58202d3406f72da0e311bea0036b23bd0a
Horace Hsieh <xiehong@marvell.com>
2011-04-26 23:49:50 +0800

    brownstone uses common board.mk
    Change-Id: Ifd1211761a0720d5221fbde105a3934bdc97fbc6
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- brownstone-froyo.mk
+++
@@ -1,6 +0,0 @@
-MANIFEST_BRANCH:=brownstone-froyo
-BOARD:=brownstone
-ANDROID_VERSION:=froyo
-PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
-
-include brownstone/board.mk
--- brownstone-gingerbread.mk
+++
@@ -1,6 +0,0 @@
-MANIFEST_BRANCH:=brownstone-gingerbread
-BOARD:=brownstone
-ANDROID_VERSION:=gingerbread
-PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
-
-include brownstone/board.mk
