da712582559ce130e44ec5dc0a9004254e9f25a6
Horace Hsieh <xiehong@marvell.com>
2011-05-05 13:28:30 +0800

    brownstone: switch build_variant to userdebug
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-brownstone-honeycomb.sh
+++ build-brownstone-honeycomb.sh
@@ -2,6 +2,6 @@ export ABS_BOARD=brownstone
 export ABS_DROID_BRANCH=honeycomb
 export ABS_PRODUCT_NAME=MMP2
 export ABS_BUILDHOST_DEF=buildhost2.def
-export ABS_DROID_VARIANT=eng
+export ABS_DROID_VARIANT=userdebug
 
 core/autobuild.sh $*
