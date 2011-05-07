f1133559379707b25097e4a28b5b64f648cd1704
Horace Hsieh <xiehong@marvell.com>
2011-05-07 18:35:24 +0800

    brownstone: specify the active branch of manifest.git
    From now on, all release/development branches of repo will share the
    same branch of manifest.git that brownstone-xxxx.
    
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-brownstone-gingerbread.sh
+++ build-brownstone-gingerbread.sh
@@ -2,5 +2,6 @@ export ABS_BOARD=brownstone
 export ABS_DROID_BRANCH=gingerbread
 export ABS_PRODUCT_NAME=MMP2
 export ABS_DROID_VARIANT=user
+export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 
 core/autobuild.sh $*
--- build-brownstone-honeycomb.sh
+++ build-brownstone-honeycomb.sh
@@ -3,5 +3,6 @@ export ABS_DROID_BRANCH=honeycomb
 export ABS_PRODUCT_NAME=MMP2
 export ABS_BUILDHOST_DEF=buildhost2.def
 export ABS_DROID_VARIANT=userdebug
+export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 
 core/autobuild.sh $*
