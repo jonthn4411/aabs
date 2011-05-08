335a3036b029da563042ad0207569b469198dda1
Horace Hsieh <xiehong@marvell.com>
2011-05-08 21:50:09 +0800

    brownstone: use the unique branch for manifest.git
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-brownstone-honeycomb.sh
+++ build-brownstone-honeycomb.sh
@@ -4,5 +4,6 @@ export ABS_PRODUCT_NAME=MMP2
 export ABS_BUILDHOST_DEF=buildhost2.def
 export ABS_DROID_VARIANT=userdebug
 export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
+export ABS_UNIQUE_MANIFEST_BRANCH=1
 
 core/autobuild.sh $*
