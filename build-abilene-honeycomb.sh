68ee8e99e8f634b39debc49ef4c36e00b3c02392
Pengcheng Guo <pcguo@marvell.com>
2011-05-11 16:41:46 +0800

    abilene: add ABS_BUILDHOST_DEF to find manifest file for honeycomb build
    Change-Id: I57c074d35377b980fe0284ec38e54859fd1376cc
    Signed-off-by: Pengcheng Guo <pcguo@marvell.com>

--- build-abilene-honeycomb.sh
+++ build-abilene-honeycomb.sh
@@ -1,6 +1,7 @@
 export ABS_BOARD=abilene
 export ABS_DROID_BRANCH=honeycomb
 export ABS_PRODUCT_NAME=MMP3
+export ABS_BUILDHOST_DEF=buildhost2.def
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 
 core/autobuild.sh $*
