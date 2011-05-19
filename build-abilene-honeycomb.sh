6bd1beb59c9f669bcbad7d8a0b838f5a91d42774
Pengcheng Guo <pcguo@marvell.com>
2011-05-19 19:34:39 +0800

    abilene: add debug console support for honeycomb
    Change-Id: I362738872ad1cdea816eaf1d3358aae91601f6d0
    Signed-off-by: Pengcheng Guo <pcguo@marvell.com>

--- build-abilene-honeycomb.sh
+++ build-abilene-honeycomb.sh
@@ -2,6 +2,7 @@ export ABS_BOARD=abilene
 export ABS_DROID_BRANCH=honeycomb
 export ABS_PRODUCT_NAME=MMP3
 export ABS_BUILDHOST_DEF=buildhost2.def
+export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 
 core/autobuild.sh $*
