867625dedcaad0d10d672834d9addc95c9de247f
Weichuan Yan <wchyan@marvell.com>
2011-05-25 16:00:05 +0800

    gforce: publish gforce auto build to Samsung folder
    Change-Id: I40c021ca74448ac4b019e05a9fc651910b3162b7
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- build-gforce-gingerbread.sh
+++ build-gforce-gingerbread.sh
@@ -1,6 +1,7 @@
 export ABS_BOARD=gforce
 export ABS_DROID_BRANCH=gingerbread
 export ABS_PRODUCT_NAME=gforce
+export ABS_BUILDHOST_DEF=buildhost3.def
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 
 core/autobuild.sh $*
