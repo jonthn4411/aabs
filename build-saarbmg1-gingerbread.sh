629c4013b6b8b857a8ebdd985d3f338156244b68
Weichuan Yan <wchyan@marvell.com>
2011-05-25 16:00:05 +0800

    saarbmg1: publish gforce auto build to Samsung folder
    Change-Id: I40c021ca74448ac4b019e05a9fc651910b3162b7
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- build-saarbmg1-gingerbread.sh
+++ build-saarbmg1-gingerbread.sh
@@ -1,6 +1,7 @@
 export ABS_BOARD=saarbmg1
 export ABS_DROID_BRANCH=gingerbread
 export ABS_PRODUCT_NAME=saarbmg1
+export ABS_BUILDHOST_DEF=buildhost3.def
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 
 core/autobuild.sh $*
