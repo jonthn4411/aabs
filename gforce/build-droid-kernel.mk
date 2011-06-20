d42e55d2af6026ff07b5264fe172dd0939a3412b
xqian <xqian@marvell.com>
2011-06-20 09:45:13 +0800

    gforce:change telephony prebuilt image name from pxafs_lyra_ext2.img to pxafs_lyra_4kb_gforce.img.onenand
    Signed-off-by: xqian <xqian@marvell.com>

--- gforce/build-droid-kernel.mk
+++ gforce/build-droid-kernel.mk
@@ -69,7 +69,7 @@ PUBLISHING_FILES_$(1)+=$(1)/symbols_lib.tgz:o:md5
 PUBLISHING_FILES_$(1)+=$(1)/build.prop:o:md5
 #PUBLISHING_FILES_$(1)+=$(1)/pxafs_lyra_4kb.img.onenand:o:md5
 PUBLISHING_FILES_$(1)+=$(1)/pxafs_lyra.img:o:md5
-PUBLISHING_FILES_$(1)+=$(1)/pxafs_lyra_ext2.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/pxafs_lyra_4kb_gforce.img.onenand:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/prebuilt_bin.gforce.tgz:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/prebuilt_bin.saarbmg2.tgz:m:md5
 PUBLISHING_FILES_$(1)+=$(1)/Boerne_DIAG_MTIL.mdb.txt:o:md5
@@ -158,7 +158,7 @@ package_droid_slc_$(1)_$(2):
 	$$(log) "  build telphony"
 	cd $(SRC_DIR)/vendor/marvell/generic/telephony/Drivers && export ANDROID_PLATFORM=$$(DROID_PRODUCT) && export MAKERULES=$(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/Rules.make && make
 	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/output/pxafs_lyra.img $$(OUTPUT_DIR)/$(2)/pxafs_lyra.img && \
-	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/output/pxafs_lyra_ext2.img $$(OUTPUT_DIR)/$(2)/pxafs_lyra_ext2.img && \
+	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/output/pxafs_lyra_4kb_gforce.img.onenand $$(OUTPUT_DIR)/$(2)/pxafs_lyra_4kb_gforce.img.onenand && \
 	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/Drivers/Boerne_DIAG*.txt $$(OUTPUT_DIR)/$(2)/ && \
 	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/prebuilt_bin.gforce $$(OUTPUT_DIR)/$(2)/ && \
 	cp -p $(SRC_DIR)/vendor/marvell/generic/telephony/prebuilt_bin.saarbmg2 $$(OUTPUT_DIR)/$(2)/ && \
