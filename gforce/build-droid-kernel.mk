3911171393e171dbceacaacce8caa6f09e90fabb
Weichuan Yan <wchyan@marvell.com>
2011-06-30 13:51:52 +0800

    gforce: add the some missing build env
    Change-Id: I59582f1fad42bba1ec9f2087abb69124aafa0011
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- gforce/build-droid-kernel.mk
+++ gforce/build-droid-kernel.mk
@@ -46,6 +46,7 @@ build_droid_root_$(1): output_dir
 	$$(log) "[$(1)]building android source code ..."
 	$$(hide)cd $$(SRC_DIR) && \
 	source ./build/envsetup.sh && \
+	export ANDROID_PLATFORM=gforce && export TARGET_MODEL=Gforce && export TARGET_BOARD_PRODUCT=gforce \
 	chooseproduct $$(DROID_PRODUCT) && choosetype $$(DROID_TYPE) && choosevariant $$(DROID_VARIANT) && \
 	ANDROID_PREBUILT_MODULES=no_kernel_modules make -j$$(MAKE_JOBS) 
 
@@ -145,6 +146,7 @@ package_droid_slc_$(1)_$(2):
 	$$(hide)export ANDROID_PREBUILT_MODULES=$$(OUTPUT_DIR)/$(2)/modules && \
 	cd $$(SRC_DIR) && \
 	source ./build/envsetup.sh && \
+	export ANDROID_PLATFORM=gforce && export TARGET_MODEL=Gforce && export TARGET_BOARD_PRODUCT=gforce \
 	chooseproduct $$(DROID_PRODUCT) && choosetype $$(DROID_TYPE) && choosevariant $$(DROID_VARIANT) && \
 	make -j$$(MAKE_JOBS) && \
 	echo "    copy  image files..." && \
