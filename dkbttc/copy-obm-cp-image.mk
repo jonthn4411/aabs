eb0aa3cfe6a8ddfcce60d8d709965d7fe170cc2a
yyli <yyli@marvell.com>
2011-07-05 13:33:45 +0800

    dkb: Publish pxa_symbols.tgz for telephony debug.
    Change-Id: If9ba144985da1e7fc28510ff2c8badfbfbc295a6
    Signed-off-by: yyli <yyli@marvell.com>

--- dkbttc/copy-obm-cp-image.mk
+++ dkbttc/copy-obm-cp-image.mk
@@ -7,6 +7,7 @@ DKBTTC_CP1:=Arbel_DIGRF3.bin
 DKBTTC_CP2:=Arbel_DIGRF3_DIAG.mdb
 DKBTTC_CP3:=Arbel_DIGRF3_NVM.mdb
 DKBTTC_CP4:=TTC1_M05_AI_A1_Flash.bin
+DKBTTC_CP5:=pxa_symbols.tgz
 DKBTTC_IMEI:=ReliableData.bin
 DKBTTC_SWD:=Software_Downloader.zip
 #DKBTTC_NO_GUI_RAMDISK:=ramdisk_no_gui.img
@@ -24,6 +25,7 @@ PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_CP1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_CP2):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_CP3):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_CP4):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_CP5):o:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_IMEI):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_SWD):m:md5
 #PUBLISHING_FILES_$(1)+=$(1)/$(DKBTTC_NO_GUI_RAMDISK):m:md5
@@ -37,6 +39,7 @@ copy_obm_cp_image_$(1):
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_CP2) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_CP3) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_CP4) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_CP5) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_IMEI) $$(OUTPUT_DIR)/$(1)
 #	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTTC_NO_GUI_RAMDISK) $$(OUTPUT_DIR)/$(1)
 	$$(log) "cp OBM and CP images  done."
