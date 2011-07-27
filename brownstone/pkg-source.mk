3256278e6c3aa0d33988ddc6143aa27fea768602
Horace Hsieh <xiehong@marvell.com>
2011-07-27 21:39:48 +0800

    brownstone: export HEAD_MANIFEST
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- brownstone/pkg-source.mk
+++ brownstone/pkg-source.mk
@@ -25,11 +25,13 @@ else
 	DROID_BASE:=shgit/honeycomb-mr2
 	KERNEL_BASE_COMMIT:=6631e089ef3dc4be98e78aafaca11047e7edf193
 	UBOOT_BASE_COMMIT:=1a2d9b30e31e2b7ed0acb64bfb2290911e3c9efb
-	export HEAD_MANIFEST=head_manifest.hc
+	HEAD_MANIFEST=head_manifest.hc
 endif
 endif
 endif
 endif
+
+export HEAD_MANIFEST
 
 .PHONY:pkgsrc
 pkgsrc: output_dir get_source_for_pkg
