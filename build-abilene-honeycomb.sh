9931c00054ef68a1c7167b7f55be3b36622fa046
Jason Ji <jcji@marvell.com>
2011-08-17 10:23:42 +0800

    abilene: unique manifest for honeycomb alpha1
    Change-Id: I85328d2865f11e4e582b575be02bbfbdbbdd0a50
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-abilene-honeycomb.sh
+++ build-abilene-honeycomb.sh
@@ -4,5 +4,6 @@ export ABS_PRODUCT_NAME=MMP3
 export ABS_BUILDHOST_DEF=buildhost2.def
 export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export ABS_UNIQUE_MANIFEST_BRANCH=1
 
 core/autobuild.sh $*
