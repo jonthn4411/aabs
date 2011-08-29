d0d1559da91c9b8ecb98b1c229716cfbf909107b
wangjunyan <wangjy@marvell.com>
2011-08-29 14:17:36 +0800

    g50: add the support for MMP2 g50 platform
    Signed-off-by: wangjunyan <wangjy@marvell.com>

---
+++ g50/build-droid-kernel.mk
@@ -0,0 +1,189 @@
+#check if the required variables have been set.
+#$(call check-variables,BUILD_VARIANTS)
+
+#
+# Include goal for build UBoot and obm
+#
+include $(BOARD)/build-uboot-obm.mk
+
+MY_SCRIPT_DIR:=$(TOP_DIR)/g50
+
+DROID_PRODUCT:=g50
+DROID_TYPE:=release
+
+KERNELSRC_TOPDIR:=kernel
+
+.PHONY:clean_droid_kernel
+clean_droid_kernel: clean_droid clean_kernel
+
+.PHONY:clean_droid
+clean_droid:
+	$(log) "clean android ..."
+	$(hide)cd $(SRC_DIR) && \
+	. $(TOP_DIR)/tools/apb $(DROID_PRODUCT) && \
+	choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	make clean
+	$(log) "    done"
+
+.PHONY:clean_kernel
+clean_kernel:
+	$(log) "clean kernel ..."
+	$(hide)cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && make clean
+	$(log) "    done"
+
+#$1:build variant
+define define-build-droid-kernel
+.PHONY:build_droid_kernel_$(1)
+build_droid_kernel_$(1): build_uboot_obm_$(1) build_kernel_$(1) build_droid_root_$(1) build_droid_pkgs_$(1)
+endef
+
+#$1:build variant
+define define-build-droid-root
+.PHONY: build_droid_root_$(1) 
+build_droid_root_$(1): output_dir
+	$$(log) "[$(1)]updating the modules..."
+	$$(hide)rm -fr $$(OUTPUT_DIR)/$(1)/modules
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
+	$$(hide)cd $$(OUTPUT_DIR)/$(1) && tar xzf modules_android_mmc.tgz
+	$$(log) "[$(1)]building android source code ..."
+	$$(hide)cd $$(SRC_DIR) && \
+	. $$(TOP_DIR)/tools/apb $$(DROID_PRODUCT) && \
+	choosetype $$(DROID_TYPE) && choosevariant $$(DROID_VARIANT) && \
+	make -j$$(MAKE_JOBS)
+	echo "    copy GPT files..." && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/primary_gpt_8g $$(OUTPUT_DIR)/$(1)/ && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/secondary_gpt_8g $$(OUTPUT_DIR)/$(1)/
+	echo "    copy ramfs files..." && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/ramdisk.img $$(OUTPUT_DIR)/$(1)/ && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/ramdisk_recovery.img $$(OUTPUT_DIR)/$(1)/
+	echo "    copy system.img ..." && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system.img $$(OUTPUT_DIR)/$(1)/
+	$$(hide)if [ -f $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata.img ]; then \
+		echo "    copy userdata.img ..." && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/userdata.img $$(OUTPUT_DIR)/$(1)/; \
+	fi
+	echo "    copy update packages..." && \
+		mkdir -p $$(OUTPUT_DIR)/$(1)/trusted && \
+		mkdir -p $$(OUTPUT_DIR)/$(1)/nontrusted && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/update_droid_trusted.zip $$(OUTPUT_DIR)/$(1)/trusted/update_droid.zip && \
+		cp -p $(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/update_droid_nontrusted.zip $$(OUTPUT_DIR)/$(1)/nontrusted/update_droid.zip
+	echo "    generating symbols_lib.tgz..." && \
+		cp -a $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/symbols/system/lib $$(OUTPUT_DIR)/$(1)/ && \
+		cd $$(OUTPUT_DIR)/$(1) && tar czf symbols_lib.tgz lib && rm lib -rf
+	$(log) "  done"
+
+PUBLISHING_FILES_$(1)+=$(1)/primary_gpt_8g:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/secondary_gpt_8g:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/ramdisk.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/ramdisk_recovery.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/system.img:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/userdata.img:o:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/update_droid.zip:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/update_droid.zip:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/symbols_lib.tgz:o:md5
+endef
+
+#$1:build variant
+define define-build-droid-pkgs
+.PHONY:build_droid_pkgs_$(1)
+build_droid_pkgs_$(1): 
+endef
+
+#$1: build variant
+#$2: internal or external
+define define-build-droid-config
+.PHONY: build_droid_$(1)_$(2)
+build_droid_$(1)_$(2): package_droid_nfs_$(1)_$(2)
+	$$(log) "build_droid_$(1)_$(2) is done, reseting the source code."
+	$$(hide)cd $$(SRC_DIR)/vendor/marvell/$$(DROID_PRODUCT)/ &&\
+		git reset --hard
+	$$(log) "  done"
+
+build_droid_pkgs_$(1): build_droid_$(1)_$(2)
+endef
+
+#$1:internal or external
+#$2:build variant
+define package-droid-nfs-config
+.PHONY: package_droid_nfs_$(1)_$(2)
+package_droid_nfs_$(1)_$(2):
+	$$(log) "[$(1)]package root file system for booting android from SD card or NFS for $(2)."
+	$$(hide)if [ -d $$(OUTPUT_DIR)/$(1)/root_nfs ]; then rm -fr $$(OUTPUT_DIR)/$(1)/root_nfs; fi
+	$$(hide)cp -r -p $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/root $$(OUTPUT_DIR)/$(1)/root_nfs && \
+	cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system $$(OUTPUT_DIR)/$(1)/root_nfs
+	$$(log) "  updating the modules..."
+	$$(hide)if [ -d $$(OUTPUT_DIR)/$(1)/modules ]; then rm -fr $$(OUTPUT_DIR)/$(1)/modules; fi
+	$$(hide)cd $$(OUTPUT_DIR)/$(1) && tar xzf modules_android_mmc.tgz && cp -r modules $$(OUTPUT_DIR)/$(1)/root_nfs/system/lib/
+	$$(log) "  modifying root nfs folder..."
+	$$(hide)cd $$(OUTPUT_DIR)/$(1)/root_nfs && $$(MY_SCRIPT_DIR)/twist_root_nfs.sh
+	$$(log) "  packaging the root_nfs.tgz..."
+	$$(hide)cd $$(OUTPUT_DIR)/$(1) && tar czf root_nfs_$(2).tgz root_nfs/
+	$$(log) "  done for package_droid_nfs_$(1)_$(2)."
+
+PUBLISHING_FILES_$(1)+=$(1)/root_nfs_$(2).tgz:m:md5
+endef
+
+#<os>:<storage>:<kernel_cfg>:<root>
+# os: the operating system
+# storage: the OS will startup from which storage
+# kernel_cfg:kernel config file used to build the kernel
+# root: optional. If specified, indicating that the kernel zImage has a root RAM file system.
+#example: android:mlc:pxa168_android_mlc_defconfig:root
+# kernel_configs:=
+#
+kernel_configs:=android:mmc:mmp2_android_defconfig 
+
+export MAKE_JOBS
+
+#$1:kernel_config
+#$2:build variant
+define define-kernel-target
+tw:=$$(subst :,  , $(1) )
+os:=$$(word 1, $$(tw) )
+storage:=$$(word 2, $$(tw) )
+kernel_cfg:=$$(word 3, $$(tw) )
+root:=$$(word 4, $$(tw) )
+
+#make sure that PUBLISHING_FILES_XXX is a simply expanded variable
+PUBLISHING_FILES_$(2)+=$(2)/zImage.$$(os):m:md5
+PUBLISHING_FILES_$(2)+=$(2)/zImage_recovery.$$(os):m:md5
+PUBLISHING_FILES_$(2)+=$(2)/vmlinux:o:md5
+PUBLISHING_FILES_$(2)+=$(2)/System.map:o:md5
+PUBLISHING_FILES_$(2)+=$(2)/modules_$$(os)_$$(storage).tgz:m:md5
+
+build_kernel_$$(os)_$$(storage)_$(2): private_os:=$$(os)
+build_kernel_$$(os)_$$(storage)_$(2): private_storage:=$$(storage)
+build_kernel_$$(os)_$$(storage)_$(2): private_root:=$$(root)
+build_kernel_$$(os)_$$(storage)_$(2): private_kernel_cfg:=$$(kernel_cfg)
+build_kernel_$$(os)_$$(storage)_$(2): private_root:=$$(root)
+build_kernel_$$(os)_$$(storage)_$(2): output_dir $$(if $$(findstring root,$$(root)), cp_$$(os)_root_dir_$$(storage)_$(2) ) 
+	$$(log) "[$(2)]starting to build kernel for booting $$(private_os) from $$(private_storage) ..."
+	$$(log) "    kernel_config: $$(private_kernel_cfg): ..."
+	$$(hide)cd $$(SRC_DIR)/$$(KERNELSRC_TOPDIR) && \
+	KERNEL_CONFIG=$$(private_kernel_cfg) make clean all 
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(2)
+	$$(log) "    copy kernel and module files ..."
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/zImage $$(OUTPUT_DIR)/$(2)/zImage.$$(private_os)
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/zImage_recovery $$(OUTPUT_DIR)/$(2)/zImage_recovery.$$(private_os)
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/kernel/vmlinux $$(OUTPUT_DIR)/$(2)
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/kernel/System.map $$(OUTPUT_DIR)/$(2)
+	$$(hide)if [ -d $$(OUTPUT_DIR)/$(2)/modules ]; then rm -fr $$(OUTPUT_DIR)/$(2)/modules; fi &&\
+	mkdir -p $$(OUTPUT_DIR)/$(2)/modules
+	$$(hide)cp $$(SRC_DIR)/$$(KERNELSRC_TOPDIR)/out/modules/* $$(OUTPUT_DIR)/$(2)/modules
+	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar czf modules_$$(private_os)_$$(private_storage).tgz modules/ 
+	$(log) "  done."
+
+.PHONY: build_kernel_$$(os)_$$(storage)_$(2)
+build_kernel_$(2): build_kernel_$$(os)_$$(storage)_$(2)
+endef
+
+$(foreach bv,$(BUILD_VARIANTS), \
+	$(eval $(call define-build-uboot-obm,$(bv)) ) \
+	$(eval $(call define-build-droid-kernel,$(bv)) ) \
+	$(foreach kc, $(kernel_configs), \
+		$(eval $(call define-kernel-target,$(kc),$(bv)) ) ) \
+	$(eval $(call define-build-droid-root,$(bv)) ) \
+	$(eval $(call define-build-droid-pkgs,$(bv)) ) \
+	$(eval $(call define-build-droid-config,$(bv),internal) ) \
+	$(eval $(call package-droid-nfs-config,$(bv),internal) ) \
+)
---
+++ g50/build-uboot-obm.mk
@@ -0,0 +1,57 @@
+#check if the required variables have been set.
+$(call check-variables,BUILD_VARIANTS)
+BOOT_SRC_DIR:=boot
+BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
+UBOOT:=u-boot.bin
+OBM_NTIM_1:=ntim_mmp2_nand_bbu_ddr.bin
+OBM_NTIM_DESC_1:=ntim_mmp2_emmc_ddr3_elipda_1g.txt
+OBM_NTLOADER_1:=MMP2_NTLOADER_3_2_19.bin
+OBM_TIM_1:=tim_mmp2_nand_bbu_ddr.bin
+OBM_TIM_DESC_1:=tim_mmp2_emmc_ddr3_elipda_1g.txt
+OBM_TLOADER_1:=MMP2_TLOADER_3_2_19.bin
+WTM_1:=Wtm_rel_mmp2.bin
+
+#$1:build variant
+define define-build-uboot-obm
+#format: <file name>:[m|o]:[md5]
+#m:means mandatory
+#o:means optional
+#md5: need to generate md5 sum
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(UBOOT):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TLOADER_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TIM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TIM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(UBOOT):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTLOADER_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTIM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/$(WTM_1):m:md5
+
+.PHONY:build_uboot_obm_$(1)
+build_uboot_obm_$(1):
+	$$(log) "starting($(1)) to build uboot and obm"
+	$$(hide)cd $$(SRC_DIR)/$$(BOOT_SRC_DIR) && \
+	make all
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/nontrusted
+
+	$$(log) "start to copy uboot and obm files"
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$$(WTM_1) $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(UBOOT) $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_TLOADER_1) $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_TIM_1) $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_TIM_DESC_1) $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(UBOOT) $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(log) "  done."
+endef
+
+.PHONY:clean_uboot_obm
+clean_uboot_obm:
+	$(log) "cleaning uboot and obm..."
+	$(hide)cd $(SRC_DIR)/$(BOOT_SRC_DIR) && \
+	make clean
+	$(log) "    done."
