3452efb1b2a78313702dd26ee09a04a993d22e40
hongzhou <hongzhou@marvell.com>
2011-10-31 16:39:05 +0800

    yellowstone: add initial variable for yellowstone platform build
    Signed-off-by: hongzhou <hongzhou@marvell.com>

---
+++ build-yellowstone-honeycomb.sh
@@ -0,0 +1,9 @@
+export ABS_BOARD=yellowstone
+export ABS_DROID_BRANCH=honeycomb
+export ABS_PRODUCT_NAME=MMP3
+export ABS_BUILDHOST_DEF=buildhost2.def
+export ABS_DROID_VARIANT=userdebug
+export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+
+core/autobuild.sh $*
+
---
+++ yellowstone/pkg-source.mk
@@ -0,0 +1,31 @@
+INTERNAL_PROJECTS +=boot/obm/.git
+#INTERNAL_PROJECTS +=boot/obm/binaries/Wtm_rel_mmp2.bin
+
+KERNEL_BASE_COMMIT:=$(KERNEL_2_6_32_BASE_COMMIT)
+UBOOT_BASE_COMMIT:=$(UBOOT_2009RC1_BASE_COMMIT)
+
+ifeq ($(ANDROID_VERSION),gingerbread)
+	KERNEL_BASE_COMMIT:=$(KERNEL_2_6_35_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201009_BASE_COMMIT)
+else
+ifeq ($(ANDROID_VERSION),honeycomb)
+	KERNEL_BASE_COMMIT:=9abd59b0df155835a970c2b9c8f93367eb793797
+	UBOOT_BASE_COMMIT:=e1b4c57096b87b4ada56df4154d9acee6a59141f
+endif
+endif
+
+#format: <file name>:[m|o]:[md5]
+#m:means mandatory
+#o:means optional
+#md5: need to generate md5 sum
+
+#
+# Sample from brownstone
+#
+#PUBLISHING_FILES+=$(ANDROID_VERSION)_RN.pdf:o
+#
+#publish_RN:
+#	$(hide)cp $(BOARD)/$(ANDROID_VERSION)_RN.pdf $(OUTPUT_DIR)
+#
+#pkgsrc: publish_RN
+#
