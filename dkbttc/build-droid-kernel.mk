7a34de9a7a6472b83ef8aa6a9c1b2dc6350f4500
Tan Jingjing <jjtan@marvell.com>
2011-11-21 00:20:22 +0800

    Fix the on longer exist directory can't copy issue
    Change-Id: Ie1fef883c518a88df3b50bbf8ea65a48a5c9b192
    Signed-off-by: Tan Jingjing <jjtan@marvell.com>

--- dkbttc/build-droid-kernel.mk
+++ dkbttc/build-droid-kernel.mk
@@ -159,12 +159,10 @@ package_droid_mmc_$(1)_$(2):
 	$$(hide)cp -r -p $$(OUTPUT_DIR)/$(2)/root $$(OUTPUT_DIR)/$(2)/root_nfs && \
 	cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/system $$(OUTPUT_DIR)/$(2)/root_nfs && \
 	cp -p -r $$(SRC_DIR)/out/target/product/$$(DROID_PRODUCT)/data $$(OUTPUT_DIR)/$(2)/root_nfs
-	#$$(hide)cp -rfp $$(SRC_DIR)/vendor/marvell/generic/ttc_telephony/drivers/output/marvell $$(OUTPUT_DIR)/$(2)/root_nfs
 	$$(log) "  updating the modules..."
 	$$(hide)if [ -d $$(OUTPUT_DIR)/$(2)/modules ]; then rm -fr $$(OUTPUT_DIR)/$(2)/modules; fi
 	$$(hide)cd $$(OUTPUT_DIR)/$(2) && tar xzf modules_android_mmc.tgz && cp -r modules $$(OUTPUT_DIR)/$(2)/root_nfs/system/lib/
 	$$(log) "  modifying root nfs folder..."
-	$$(hide)cd $$(OUTPUT_DIR)/$(2)/root_nfs && $$(MY_SCRIPT_DIR)/twist_root_nfs.sh
 	$$(log) "copy demo media files to /sdcard if there are demo media files..."
 	$$(hide)if [ -d "$$(DEMO_MEDIA_DIR)" ]; then \
 			mkdir -p $$(OUTPUT_DIR)/$(2)/root_nfs/sdcard && \
