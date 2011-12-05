99fc6642ac7762465aea1bdb20fe86cb293abd57
Hong Zhou <hongzhou@marvell.com>
2011-12-05 14:13:14 +0800

    abilene:add emmc_cm for cm(coremorphall) integrated on mmp3
    Signed-off-by: Hong Zhou <hongzhou@marvell.com>

--- abilene/build-uboot-obm.mk
+++ abilene/build-uboot-obm.mk
@@ -1,8 +1,11 @@
 #check if the required variables have been set.
+
 $(call check-variables,BUILD_VARIANTS)
 BOOT_SRC_DIR:=boot
 BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
+BOOT_OUT_CM_DIR:=$(BOOT_OUT_DIR)/CM
 UBOOT:=u-boot.bin
+UBOOT_CM:=u-boot_cm.bin
 OBM_DESC_1:=ntim_mmp3_v7_mp_emmc_dis_uboot.txt
 OBM_NTIM_1:=ntim_mmp3_v7_mp_emmc_dis_uboot.bin
 OBM_DESC_2:=ntim_mmp3_v7_mp_emmc_pop_uboot.txt
@@ -17,7 +20,12 @@ OBM_DESC_6:=ntim_mmp3_v7_mp_emmc_pop_uboot_1000.txt
 OBM_NTIM_6:=ntim_mmp3_v7_mp_emmc_pop_uboot_1000.bin
 OBM_DESC_7:=ntim-mmp3-yellowstone-dis-800.txt
 OBM_NTIM_7:=ntim-mmp3-yellowstone-dis-800.bin
+OBM_DESC_8:=ntim_abex_dis-797-797-399-399-532-532-399-200__3_2_sm_533_1200mv.txt
+OBM_NTIM_8:=ntim_abex_dis-797-797-399-399-532-532-399-200__3_2_sm_533_1200mv.bin
+OBM_DESC_9:=ntim_abex_dis-797-797-399-399-532-532-399-200__3_2_sm_533_1225mv.txt
+OBM_NTIM_9:=ntim_abex_dis-797-797-399-399-532-532-399-200__3_2_sm_533_1225mv.bin
 WTM_BIN:=Wtm_rel_mmp3.bin
+CM_BIN:=coremorphall.bin
 
 #$1:build variant
 define define-build-uboot-obm
@@ -25,7 +33,7 @@ define define-build-uboot-obm
 #m:means mandatory
 #o:means optional
 #md5: need to generate md5 sum
-PUBLISHING_FILES+=$(1)/$(UBOOT):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/$(UBOOT):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTIM_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_DESC_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTIM_2):m:md5
@@ -41,7 +49,13 @@ PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_DESC_6):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_NTIM_7):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(OBM_DESC_7):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/emmc/$(WTM_BIN):m:md5
-
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(OBM_NTIM_8):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(OBM_DESC_8):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(OBM_NTIM_9):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(OBM_DESC_9):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(WTM_BIN):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(CM_BIN):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/$(UBOOT_CM):m:md5
 
 
 .PHONY:build_uboot_obm_$(1)
@@ -51,9 +65,10 @@ build_uboot_obm_$(1):
 	make all
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc_cm
 
 	$$(log) "start to copy uboot and obm files"
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_DESC_1) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTIM_2) $$(OUTPUT_DIR)/$(1)/emmc
@@ -69,6 +84,13 @@ build_uboot_obm_$(1):
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_NTIM_7) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(OBM_DESC_7) $$(OUTPUT_DIR)/$(1)/emmc
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/emmc/$$(WTM_BIN) $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(OBM_NTIM_8) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(OBM_DESC_8) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(OBM_NTIM_9) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(OBM_DESC_9) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(WTM_BIN) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(CM_BIN) $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/emmc/$$(UBOOT_CM) $$(OUTPUT_DIR)/$(1)/emmc_cm
 	$$(log) "  done."
 
 endef
