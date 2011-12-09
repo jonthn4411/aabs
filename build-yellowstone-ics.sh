a00c45a8985152980c238675ed74cd4ed6557168
Jason Ji <jcji@marvell.com>
2011-12-09 13:54:26 +0800

    yellowstone: use /git/android/... as repos
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-yellowstone-ics.sh
+++ build-yellowstone-ics.sh
@@ -1,7 +1,7 @@
 export ABS_BOARD=yellowstone
 export ABS_DROID_BRANCH=ics
 export ABS_PRODUCT_NAME=MMP3
-export ABS_BUILDHOST_DEF=buildhost2.def
+export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 
