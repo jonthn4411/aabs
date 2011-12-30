2c739f52f0a3bac5ff38a6321c78cba98dec6486
Tan Jingjing <jjtan@marvell.com>
2011-12-30 13:09:01 +0800

    dkbtd: copy README.ics or README.gb to be README
    Change-Id: Iccc191a3f875d1ec89825e1e4c130fa61730a7cb
    Signed-off-by: Tan Jingjing <jjtan@marvell.com>

--- dkbtd/pkg-source.mk
+++ dkbtd/pkg-source.mk
@@ -73,13 +73,13 @@ pkgsrc: output_dir get_source_for_pkg
 	$(hide)cp $(TOP_DIR)/core/setup_android.sh $(OUTPUT_DIR)
 	$(hide)if [ "$(ANDROID_VERSION)" = "ics" ];then \
 			cp $(BOARD)/ReleaseNotes.ics $(OUTPUT_DIR)/ReleaseNotes.txt && \
-			ln -s $(BOARD)/README.ics $(BOARD)/README; \
+			cp $(BOARD)/README.ics $(BOARD)/README; \
 		elif [ "$(ANDROID_VERSION)" = "gingerbread" ]; then \
 			cp $(BOARD)/ReleaseNotes.gb $(OUTPUT_DIR)/ReleaseNotes.txt && \
-			ln -s $(BOARD)/README.gb $(BOARD)/README; \
+			cp $(BOARD)/README.gb $(BOARD)/README; \
 		else \
 			cp $(BOARD)/ReleaseNotes.txt $(OUTPUT_DIR)/ && \
-			ln -s $(BOARD)/README.txt $(BOARD)/README; \
+			cp $(BOARD)/README.txt $(BOARD)/README; \
 		fi
 	$(log) "  done."
 
