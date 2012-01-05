6c1881b4ca089e1fe7c300b295d108395bf939e2
Tan Jingjing <jjtan@marvell.com>
2012-01-05 20:59:11 +0800

    dkbtd-ics: change the building mode from eng to userdebug
    Change-Id: Ibe0d872269f55de9e856ff7681c9b2ad4573fc04
    Signed-off-by: Tan Jingjing <jjtan@marvell.com>

--- build-dkbtd-ics.sh
+++ build-dkbtd-ics.sh
@@ -3,7 +3,7 @@ export ABS_DROID_BRANCH=ics
 export ABS_PRODUCT_NAME=dkbtd
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 export ABS_BUILDHOST_DEF=buildhost.def
-export ABS_DROID_VARIANT=eng
+export ABS_DROID_VARIANT=userdebug
 export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 export ABS_UNIQUE_MANIFEST_BRANCH=1
 
