a3443d25580e246e06e0d93a27de3197b6e3755b
Tan Jingjing <jjtan@marvell.com>
2012-01-12 13:26:54 +0800

    fix download from master branch issue
    Change-Id: I6f02aa5be1aa09c067bc30a26ae99f6112d940a2
    Signed-off-by: Tan Jingjing <jjtan@marvell.com>

--- build-dkbtd-ics.sh
+++ build-dkbtd-ics.sh
@@ -4,6 +4,5 @@ export ABS_PRODUCT_NAME=dkbtd
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
-export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 
 core/autobuild.sh $*
