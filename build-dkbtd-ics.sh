42d4a12b79d315ba80d72501b0ed1831d1821597
Tan Jingjing <jjtan@marvell.com>
2012-01-12 09:42:39 +0800

    rls_dkbtd_wifi_direct: remove the unique mode
    Change-Id: I05052e41d44920b297e1f8c0550336e79652ba3b
    Signed-off-by: Tan Jingjing <jjtan@marvell.com>

--- build-dkbtd-ics.sh
+++ build-dkbtd-ics.sh
@@ -5,6 +5,5 @@ export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
 export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
-export ABS_UNIQUE_MANIFEST_BRANCH=1
 
 core/autobuild.sh $*
