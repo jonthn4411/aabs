bd7cebc6e873254611eda6f9c55af98f21dd2302
Horace Hsieh <xiehong@marvell.com>
2012-01-20 18:26:07 +0800

    mmp2: publish dxtim_platform_uImage if any
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- brownstone/build-uboot-obm.mk
+++ brownstone/build-uboot-obm.mk
@@ -9,12 +9,14 @@ OBM_NTIM_DESC_1:=ntim_platform_512m_ddr3.txt
 OBM_NTLOADER_1:=MMP2_LINUX_ARM_BL_3_2_21_EB_JO.bin
 OBM_DNTIM_1:=dntim_platform.bin
 OBM_DNTIM_DESC_1:=dntim_platform.txt
+OBM_DNTIM_DESC_2:=dntim_platform_uImage.txt
 
 OBM_TIM_1:=tim_platform_512m_ddr3.bin
 OBM_TIM_DESC_1:=tim_platform_512m_ddr3.txt
 OBM_TLOADER_1:=MMP2_LINUX_ARM_BL_3_2_21_TRUSTED_EB_JO.bin
 OBM_DTIM_1:=dtim_platform.bin
 OBM_DTIM_DESC_1:=dtim_platform.txt
+OBM_DTIM_DESC_2:=dtim_platform_uImage.txt
 OBM_DTIM_KEY_1:=EncryptKey.txt
 
 WTM_1:=Wtm_rel_mmp2.bin
@@ -30,14 +32,16 @@ PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TLOADER_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TIM_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_TIM_DESC_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_DTIM_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_DTIM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_DTIM_DESC_1):o:md5
+PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_DTIM_DESC_2):o:md5
 PUBLISHING_FILES_$(1)+=$(1)/trusted/$(OBM_DTIM_KEY_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(UBOOT):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTLOADER_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTIM_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_NTIM_DESC_1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_DNTIM_1):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_DNTIM_DESC_1):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_DNTIM_DESC_1):o:md5
+PUBLISHING_FILES_$(1)+=$(1)/nontrusted/$(OBM_DNTIM_DESC_2):o:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(WTM_1):m:md5
 
 .PHONY:build_uboot_obm_$(1)
@@ -59,14 +63,16 @@ build_uboot_obm_$(1):
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_TIM_1) $$(OUTPUT_DIR)/$(1)/trusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_TIM_DESC_1) $$(OUTPUT_DIR)/$(1)/trusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_1) $$(OUTPUT_DIR)/$(1)/trusted
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)if [ -f $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_DESC_1) ]; then cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/trusted; fi
+	$$(hide)if [ -f $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_DESC_2) ]; then cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_DESC_2) $$(OUTPUT_DIR)/$(1)/trusted; fi
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/trusted/$$(OBM_DTIM_KEY_1) $$(OUTPUT_DIR)/$(1)/trusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(UBOOT) $$(OUTPUT_DIR)/$(1)/nontrusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTLOADER_1) $$(OUTPUT_DIR)/$(1)/nontrusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTIM_1) $$(OUTPUT_DIR)/$(1)/nontrusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_NTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/nontrusted
 	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_1) $$(OUTPUT_DIR)/$(1)/nontrusted
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)if [ -f $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_DESC_1) ]; then cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_DESC_1) $$(OUTPUT_DIR)/$(1)/nontrusted; fi
+	$$(hide)if [ -f $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_DESC_2) ]; then cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/nontrusted/$$(OBM_DNTIM_DESC_2) $$(OUTPUT_DIR)/$(1)/nontrusted; fi
 	$$(log) "  done."
 endef
 
