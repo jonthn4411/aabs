7a02495111870113ce585d5df5b5ad19a3738906
Jason Ji <jcji@marvell.com>
2012-02-15 12:01:47 +0800

    yellowstone: synced with abilene
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- yellowstone/build-uboot-obm.mk
+++ yellowstone/build-uboot-obm.mk
@@ -1,9 +1,11 @@
 #check if the required variables have been set.
+
 $(call check-variables,BUILD_VARIANTS)
 BOOT_SRC_DIR:=boot
 BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
-UBOOT:=u-boot.bin
-NTIM_BIN_TGZ:=ntim_bin.tgz
+BOOT_OUT_CM_DIR:=$(BOOT_OUT_DIR)/CM
+BOOT_OUT_NOR_DIR:=$(BOOT_OUT_DIR)/NORMAL
+
 
 #$1:build variant
 define define-build-uboot-obm
@@ -11,9 +13,13 @@ define define-build-uboot-obm
 #m:means mandatory
 #o:means optional
 #md5: need to generate md5 sum
-PUBLISHING_FILES+=$(1)/$(UBOOT):m:md5
-PUBLISHING_FILES+=$(1)/Wtm_rel_mmp3.bin:m:md5
-PUBLISHING_FILES+=$(1)/$(NTIM_BIN_TGZ):m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/u-boot.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/ntim.bin.tgz:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/u-boot.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/coremorphall.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/ntim.bin.tgz:m:md5
 
 .PHONY:build_uboot_obm_$(1)
 build_uboot_obm_$(1):
@@ -22,13 +28,21 @@ build_uboot_obm_$(1):
 	make all
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc/ntim
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc_cm/ntim
 
 	$$(log) "start to copy uboot and obm files"
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)
-	$$(hide)cd $$(SRC_DIR)/$$(BOOT_OUT_DIR)/ && tar czvf $(NTIM_BIN_TGZ) ntim
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_DIR)/$(NTIM_BIN_TGZ) $$(OUTPUT_DIR)/$(1)
-	$$(log) "  done."
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/ntim/* $$(OUTPUT_DIR)/$(1)/emmc/ntim/
+	$$(hide)cd $$(OUTPUT_DIR)/$(1)/emmc/ && tar zcvf ntim.bin.tgz ntim
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/coremorphall.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/ntim/* $$(OUTPUT_DIR)/$(1)/emmc_cm/ntim/
+	$$(hide)cd $$(OUTPUT_DIR)/$(1)/emmc_cm/ && tar zcvf ntim.bin.tgz ntim
+	$$(log) "    done."
 
 endef
 
