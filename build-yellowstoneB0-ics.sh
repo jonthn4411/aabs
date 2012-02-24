504f8545096a6a30f3abcd4a941eab61cb8d2b75
Jason Ji <jcji@marvell.com>
2012-02-24 13:19:49 +0800

    mmp3: add MMP3_VARIANT to identify MMP3 devices
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-yellowstoneB0-ics.sh
+++ build-yellowstoneB0-ics.sh
@@ -4,6 +4,7 @@ export ABS_PRODUCT_NAME=MMP3
 export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export MMP3_VARIANT=yellowstoneB0
 
 core/autobuild.sh $*
 
