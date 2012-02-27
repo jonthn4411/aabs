dc363bb8e4c1331729c94b15eeac3dce6266522f
Jason Ji <jcji@marvell.com>
2012-02-27 10:25:08 +0800

    abilene: add abileneB0 branch
    Signed-off-by: Jason Ji <jcji@marvell.com>

---
+++ build-abileneB0-ics.sh
@@ -0,0 +1,10 @@
+export ABS_BOARD=yellowstoneB0
+export ABS_DROID_BRANCH=ics
+export ABS_PRODUCT_NAME=MMP3
+export ABS_BUILDHOST_DEF=buildhost.def
+export ABS_DROID_VARIANT=userdebug
+export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export MMP3_VARIANT=abileneB0
+
+core/autobuild.sh $*
+
