ed75bd0e47ac8e0f2592fdf2582bbc39017f01df
Johnny He <yhe9@marvell.com>
2012-02-29 18:31:52 +0800

    saarcnevo/dkbnevo:fix release branch sync wrong code issue
    Signed-off-by: Johnny He <yhe9@marvell.com>

--- build-saarcnevo-ics.sh
+++ build-saarcnevo-ics.sh
@@ -4,7 +4,6 @@ export ABS_PRODUCT_NAME=saarcnevo
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
-export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 export ABS_UNIQUE_MANIFEST_BRANCH=1
 
 core/autobuild.sh $*
