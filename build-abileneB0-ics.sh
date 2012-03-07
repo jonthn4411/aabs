bf54cd9d69f7556c4be790d121fece195f6ee69a
Jason Ji <jcji@marvell.com>
2012-03-07 19:10:59 +0800

    abileneB0: set its variant to abilene so to build the right uboot
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-abileneB0-ics.sh
+++ build-abileneB0-ics.sh
@@ -4,7 +4,7 @@ export ABS_PRODUCT_NAME=MMP3
 export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
-export MMP3_VARIANT=abileneB0
+export MMP3_VARIANT=abilene
 
 core/autobuild.sh $*
 
