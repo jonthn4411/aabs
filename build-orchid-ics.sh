fa68fed5aa38fed3ca8eabeb32ba77fd4fcc7ec0
Jason Ji <jcji@marvell.com>
2012-03-13 15:11:41 +0800

    orchid: fix a typo
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-orchid-ics.sh
+++ build-orchid-ics.sh
@@ -5,7 +5,7 @@ export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_DROID_VARIANT=userdebug
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 export MMP3_VARIANT=orchid
-export DROID_PRODUCT = orchid
+export DROID_PRODUCT=orchid
 
 core/autobuild.sh $*
 
