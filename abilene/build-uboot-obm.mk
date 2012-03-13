dfdc622dca8b4965e8b7370187cdad6d5836593c
Jason Ji <jcji@marvell.com>
2012-03-13 14:29:48 +0800

    Revert "mmp3: fix a bug, CM/NORMAL dirs should have been replaced"
    This reverts commit 384b50fcad7a5e00e3dcc583163c79aeec9edc29.

--- abilene/build-uboot-obm.mk
+++ abilene/build-uboot-obm.mk
@@ -3,8 +3,8 @@
 $(call check-variables,BUILD_VARIANTS)
 BOOT_SRC_DIR:=boot
 BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
-BOOT_OUT_TRUSTED_DIR:=$(BOOT_OUT_DIR)/trusted
-BOOT_OUT_NONTRUSTED_DIR:=$(BOOT_OUT_DIR)/nontrusted
+BOOT_OUT_CM_DIR:=$(BOOT_OUT_DIR)/CM
+BOOT_OUT_NOR_DIR:=$(BOOT_OUT_DIR)/NORMAL
 
 
 #$1:build variant
@@ -33,14 +33,14 @@ build_uboot_obm_$(1):
 	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/trusted/ntim
 
 	$$(log) "start to copy uboot and obm files"
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NONTRUSTED_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/nontrusted
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NONTRUSTED_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/nontrusted
-	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_NONTRUSTED_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/nontrusted/
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/nontrusted
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/nontrusted/
 	$$(hide)cd $$(OUTPUT_DIR)/$(1)/nontrusted/ && tar zcvf ntim.bin.tgz ntim
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_TRUSTED_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/trusted
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_TRUSTED_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/trusted
-	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_TRUSTED_DIR)/coremorphall.bin $$(OUTPUT_DIR)/$(1)/trusted
-	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_TRUSTED_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/trusted/
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/coremorphall.bin $$(OUTPUT_DIR)/$(1)/trusted
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/trusted/
 	$$(hide)cd $$(OUTPUT_DIR)/$(1)/trusted/ && tar zcvf ntim.bin.tgz ntim
 	$$(log) "    done."
 
