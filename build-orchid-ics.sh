8ca3474a254f2238d0b5e9b014421cc5590e3819
Jason Ji <jcji@marvell.com>
2012-03-13 15:07:27 +0800

    mmp3: a new product, orchid
    Signed-off-by: Jason Ji <jcji@marvell.com>

---
+++ build-orchid-ics.sh
@@ -0,0 +1,11 @@
+export ABS_BOARD=orchid
+export ABS_DROID_BRANCH=ics
+export ABS_PRODUCT_NAME=MMP3
+export ABS_BUILDHOST_DEF=buildhost.def
+export ABS_DROID_VARIANT=userdebug
+export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export MMP3_VARIANT=orchid
+export DROID_PRODUCT = orchid
+
+core/autobuild.sh $*
+
