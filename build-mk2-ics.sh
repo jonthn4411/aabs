bb906c3a4eafe670e0971030878c055ec1d406ce
Jason Ji <jcji@marvell.com>
2012-03-15 16:23:52 +0800

    mk2: add mk2 product
    Signed-off-by: Jason Ji <jcji@marvell.com>

---
+++ build-mk2-ics.sh
@@ -0,0 +1,11 @@
+export ABS_BOARD=mk2
+export ABS_DROID_BRANCH=ics
+export ABS_PRODUCT_NAME=MMP3
+export ABS_BUILDHOST_DEF=buildhost.def
+export ABS_DROID_VARIANT=userdebug
+export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export MMP3_VARIANT=mk2
+export DROID_PRODUCT=mk2
+
+core/autobuild.sh $*
+
