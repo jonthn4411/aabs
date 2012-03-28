d97cd995307f4bc5e2ffdded12413f07614b1e5b
Jason Ji <jcji@marvell.com>
2012-03-28 13:06:46 +0800

    orchid: add telephony binaries
    Signed-off-by: Jason Ji <jcji@marvell.com>

---
+++ orchid/build-uboot-obm.mk
@@ -0,0 +1,60 @@
+#check if the required variables have been set.
+
+$(call check-variables,BUILD_VARIANTS)
+BOOT_SRC_DIR:=boot
+BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
+BOOT_OUT_CM_DIR:=$(BOOT_OUT_DIR)/CM
+BOOT_OUT_NOR_DIR:=$(BOOT_OUT_DIR)/NORMAL
+
+
+#$1:build variant
+define define-build-uboot-obm
+#format: <file name>:[m|o]:[md5]
+#m:means mandatory
+#o:means optional
+#md5: need to generate md5 sum
+PUBLISHING_FILES_$(1)+=$(1)/emmc/u-boot.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc/ntim.bin.tgz:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/u-boot.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/coremorphall.bin:m:md5
+PUBLISHING_FILES_$(1)+=$(1)/emmc_cm/ntim.bin.tgz:m:md5
+
+.PHONY:build_uboot_obm_$(1)
+build_uboot_obm_$(1):
+	$$(log) "starting($(1)) to build uboot and obm"
+	$$(hide)cd $$(SRC_DIR)/$$(BOOT_SRC_DIR) && \
+	make all
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc/ntim
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)mkdir -p $$(OUTPUT_DIR)/$(1)/emmc_cm/ntim
+
+	$$(log) "start to copy uboot and obm files"
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/emmc
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_NOR_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/emmc/
+	$$(hide)cd $$(OUTPUT_DIR)/$(1)/emmc/ && tar zcvf ntim.bin.tgz ntim
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/u-boot.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/Wtm_rel_mmp3.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/coremorphall.bin $$(OUTPUT_DIR)/$(1)/emmc_cm
+	$$(hide)cp -rf $$(SRC_DIR)/$$(BOOT_OUT_CM_DIR)/ntim/ $$(OUTPUT_DIR)/$(1)/emmc_cm/
+	$$(hide)cd $$(OUTPUT_DIR)/$(1)/emmc_cm/ && tar zcvf ntim.bin.tgz ntim
+	$$(log) "    done."
+
+endef
+
+$(foreach bv, $(BUILD_VARIANTS), $(eval $(call define-build-uboot-obm,$(bv)) ) )
+
+.PHONY:clean_uboot_obm
+clean_uboot_obm:
+	$(log) "cleaning uboot and obm..."
+	$(hide)cd $(SRC_DIR)/$(BOOT_SRC_DIR) && \
+	make clean
+	$(log) "    done."
+
+
+
+
---
+++ orchid/twist_root_nfs.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+# modify the root_nfs folder for booting Android from NFS properly.
+# The current folder running this script must be the folder of root nfs.
+# This script can be run multiple times on the root nfs folder and still
+# generate the proper result.
+
+function gen_init_nfs_sh()
+{
+	cat > $1 <<-EOF
+	#!/system/bin/sh
+	
+	#simulate SD card and generate mounted intent
+	chmod 0777 /sdcard
+	chmod 0444 /sdcard/*.*
+	setprop EXTERNAL_STORAGE_STATE mounted
+	setprop fake.sdcard.state true	
+	#touch the directory. A trick for NFS
+	ls /sdcard >/dev/null	
+	sleep 2s
+	am broadcast -a android.intent.action.MEDIA_MOUNTED --ez read-only false -d file:///sdcard
+
+	#workaround for keychars
+	chmod 0644 /system/usr/keychars/*
+
+	EOF
+}
+
+echo "  modifying init.rc..." &&
+sed -i "/^[ tab]*mount[ tab]*.\+[ tab]*\(\/system\|\/data\|\/cache\)/ s/mount/#(for nfs)mount/" init.rc &&
+sed -i "/^[ tab]*mount rootfs rootfs/ s/mount/#(for nfs)mount/" init.rc &&
+sed -i '/^[ tab]*mkdir \/sdcard 0000 system system/ {
+s/mkdir/#(for nfs)mkdir/ 
+a\
+\
+    #>>>for nfs\
+    mkdir \/sdcard 0777 system system\
+    chmod 0777 \/sdcard\
+    export amsd \"am broadcast -a android.intent.action.MEDIA_MOUNTED --ez read-only false -d file:\/\/\/sdcard\"\
+    #<<<for nfs\
+
+}' init.rc &&
+
+#add service to init nfs
+if grep "service init-nfs /init.nfs.sh" init.rc >/dev/null; then
+  echo "  already have service init-nfs defined..."
+else
+  cat >>init.rc <<-EOF
+	
+	#for nfs
+	service init-nfs /init.nfs.sh
+	    user root
+	    group root
+	    oneshot
+	#for nfs
+	
+	EOF
+fi &&
+
+gen_init_nfs_sh init.nfs.sh &&
+chmod 0755 init.nfs.sh &&
+
+#don't mount sdcard in vold for NFS
+echo "  disable mount sdcard in vold.conf"
+
+if [ -e "system/etc/vold.conf" ]; then
+sed -i '/^[ tab]*volume_sdcard/,/\}/ s/\(.*\)/#\1/' system/etc/vold.conf
+else
+if [ -e "system/etc/vold.fstab" ]; then
+sed -i '/[ tab]*dev_mount sdcard \/mnt\/sdcard/ s/dev_mount/#(for nfs)dev_mount/' system/etc/vold.fstab
+fi
+fi
+
+if [ -e "system/etc/dhcpcd/dhcpcd-run-hooks" ]; then
+chmod +x system/etc/dhcpcd/dhcpcd-run-hooks
+fi
+
+if [ $? -ne 0 ]; then
+  exit 1
+else
+  exit 0
+fi
+
+
