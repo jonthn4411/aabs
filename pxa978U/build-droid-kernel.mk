0982b9d722f4085ba0f772dba9c6a82c927f0d9e
Johnny He <yhe9@marvell.com>
2012-04-10 13:10:46 +0800

    pxa978U: initial code on new autobuild system
    Signed-off-by: Johnny He <yhe9@marvell.com>

---
+++ pxa978U/build-droid-kernel.mk
@@ -0,0 +1,193 @@
+#check if the required variables have been set.
+$(call check-variables, ABS_SOC ABS_DROID_BRANCH ABS_DROID_VARIANT)
+
+MY_SCRIPT_DIR:=$(TOP_DIR)/$(ABS_SOC)
+
+DROID_TYPE:=release
+DROID_VARIANT:=$(ABS_DROID_VARIANT)
+
+KERNELSRC_TOPDIR:=kernel
+
+
+
+define define-clean-droid-kernel-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY:clean_droid_kernel_$$(product)
+clean_droid_kernel_$$(product): clean_droid_$$(product) clean_kernel_$$(product)
+
+.PHONY:clean_droid_$$(product)
+clean_droid_$$(product): private_product:=$$(product)
+clean_droid_$$(product): private_device:=$$(device)
+clean_droid_$$(product):
+	$(log) "clean android ..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh &&
+	chooseproduct $(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	make clean
+	$(log) "    done"
+
+.PHONY:clean_kernel_$$(product)
+clean_kernel_$$(product): private_product:=$$(product)
+clean_kernel_$$(product): private_device:=$$(device)
+clean_kernel_$$(product):
+	$(log) "clean kernel ..."
+	$(hide)cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && make clean
+	$(log) "    done"
+endef
+
+#we need first build the android, so we get the root dir 
+# and then we build the kernel images with the root dir and get the package of corresponding modules
+# and then we use those module package to build corresponding android package.
+
+define define-build-droid-kernel-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY:build_droid_kernel_$$(product)
+build_droid_kernel_$$(product): build_kernel_$$(product) build_droid_$$(product) build_telephony_$$(product) build_droid_update_pkgs_$$(product)
+endef
+
+export KERNEL_TOOLCHAIN_PREFIX
+export MAKE_JOBS
+
+#$1:kernel_config
+#$2:build variant
+define define-build-kernel-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY: build_kernel_$$(product)
+
+#make sure that PUBLISHING_FILES_XXX is a simply expanded variable
+PUBLISHING_FILES+=$$(product)/zImage:m:md5
+PUBLISHING_FILES+=$$(product)/zImage-recovery:m:md5
+PUBLISHING_FILES+=$$(product)/vmlinux:o:md5
+PUBLISHING_FILES+=$$(product)/System.map:o:md5
+build_kernel_$$(product): private_product:=$$(product)
+build_kernel_$$(product): private_device:=$$(device)
+build_kernel_$$(product): output_dir
+	$(log) "[$$(private_product)]starting to build kernel ..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && \
+	make kernel
+	$(log) "[$$(private_product)]starting to build modules ..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && \
+	make modules
+	$(hide)mkdir -p $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp $(SRC_DIR)/$(KERNELSRC_TOPDIR)/out/zImage $(OUTPUT_DIR)/$$(private_product)/
+	$(hide)cp $(SRC_DIR)/$(KERNELSRC_TOPDIR)/out/zImage-recovery $(OUTPUT_DIR)/$$(private_product)/
+	$(hide)cp $(SRC_DIR)/$(KERNELSRC_TOPDIR)/kernel/vmlinux $(OUTPUT_DIR)/$$(private_product)/
+	$(hide)cp $(SRC_DIR)/$(KERNELSRC_TOPDIR)/kernel/System.map $(OUTPUT_DIR)/$$(private_product)/
+	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/modules ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/modules; fi &&\
+	mkdir -p $(OUTPUT_DIR)/$$(private_product)/modules
+	$(hide)cp $(SRC_DIR)/$(KERNELSRC_TOPDIR)/out/modules/* $(OUTPUT_DIR)/$$(private_product)/modules
+	$(log) "  done."
+endef
+
+##!!## build rootfs for android, make -j4 android, copy root, copy ramdisk/userdata/system.img to outdir XXX
+#$1:build variant
+define define-build-droid-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY: build_droid_$$(product)
+build_droid_$$(product): private_product:=$$(product)
+build_droid_$$(product): private_device:=$$(device)
+build_droid_$$(product): build_kernel_$$(product)
+	$(log) "[$$(private_product)] building android source code ..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	make -j$(MAKE_JOBS)
+
+	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/root ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/root; fi
+	$(hide)echo "  copy root directory ..." 
+	$(hide)mkdir -p $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/out/target/product/$$(private_device)/root $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/out/target/product/$$(private_device)/ramdisk.img $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/out/target/product/$$(private_device)/ramdisk-recovery.img $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/out/target/product/$$(private_device)/userdata.img $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/out/target/product/$$(private_device)/system.img $(OUTPUT_DIR)/$$(private_product)
+	$(log) "  done"
+##!!## first time publish: all for two
+PUBLISHING_FILES+=$$(product)/userdata.img:m:md5
+PUBLISHING_FILES+=$$(product)/system.img:m:md5
+PUBLISHING_FILES+=$$(product)/ramdisk.img:m:md5
+PUBLISHING_FILES+=$$(product)/symbols_system.tgz:o:md5
+PUBLISHING_FILES+=$$(product)/ramdisk-recovery.img:m:md5
+PUBLISHING_FILES+=$$(product)/build.prop:o:md5
+endef
+
+
+define define-build-telephony-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY: build_telephony_$$(product)
+ifeq ($(ABS_DROID_BRANCH),ics)
+PUBLISHING_FILES+=$$(product)/Arbel_DKB_SKWS_DIAG.mdb:m:md5
+PUBLISHING_FILES+=$$(product)/Arbel_REL7_PMD2NONE.bin:m:md5
+PUBLISHING_FILES+=$$(product)/Arbel_REL7_PMD2NONE_DIAG.mdb:m:md5
+PUBLISHING_FILES+=$$(product)/Arbel_REL7_PMD2NONE_NVM.mdb:m:md5
+PUBLISHING_FILES+=$$(product)/NEVO_C0_Flash.bin:m:md5
+PUBLISHING_FILES+=$$(product)/nvm_ext2.img:m:md5
+PUBLISHING_FILES+=$$(product)/plugin_LYRA5V03_BANDS128.bin:m:md5
+PUBLISHING_FILES+=$$(product)/pxafs_lyra_ext2.img:m:md5
+PUBLISHING_FILES+=$$(product)/pxafs_symbols.tgz:m:md5
+PUBLISHING_FILES+=$$(product)/Boerne_DIAG_MTIL.mdb.txt:m:md5
+endif
+
+build_telephony_$$(product): private_product:=$$(product)
+build_telephony_$$(product): private_device:=$$(device)
+build_telephony_$$(product): build_droid_$$(product)
+	$(log) "[$$(private_product)]starting to build telephony..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && \
+	make telephony
+
+	$$(hide)mkdir -p $(OUTPUT_DIR)/$$(private_product)
+	$$(log) "    copy telephony files ..."
+	$$(hide)if [ -d $(SRC_DIR)/$(KERNELSRC_TOPDIR)/out/telephony ]; then cp -a $(SRC_DIR)/$(KERNELSRC_TOPDIR)/out/telephony/* /$(OUTPUT_DIR)/$$(private_product); fi
+	$(log) "  done."
+endef
+
+define define-build-droid-update-pkgs
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY: build_droid_update_pkgs_$$(product)
+build_droid_update_pkgs_$$(product): private_product:=$$(product)
+build_droid_update_pkgs_$$(product): private_device:=$$(device)
+build_droid_update_pkgs_$$(product): build_uboot_obm_$$(product)
+	$$(log) "[$$(private_product)]generating update packages..."
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	make mrvlotapackage
+	echo "    copy update packages..." && \
+		mkdir -p $$(OUTPUT_DIR)/$(1) && \
+		cp -p $(SRC_DIR)/out/target/product/$$(private_device)/pxa978saarc_def-ota-mrvl.zip $(OUTPUT_DIR)/$$(private_product)/pxa978saarc_def-ota-mrvl.zip
+	$(log) "  done"
+
+PUBLISHING_FILES+=$$(product)/pxa978saarc_def-ota-mrvl.zip:m:md5
+endef
+
+
+
+
+$(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call define-build-droid-kernel-target,$(bv)) )\
+				$(eval $(call define-build-kernel-target,$(bv)) ) \
+				$(eval $(call define-build-droid-target,$(bv)) ) \
+				$(eval $(call define-build-telephony-target,$(bv)) ) \
+				$(eval $(call define-clean-droid-kernel-target,$(bv)) ) \
+				$(eval $(call define-build-droid-update-pkgs,$(bv)) ) \
+)
---
+++ pxa978U/build-uboot-obm.mk
@@ -0,0 +1,77 @@
+#check if the required variables have been set.
+#$(call check-variables,BUILD_VARIANTS)
+BOOT_SRC_DIR:=boot
+BOOT_OUT_DIR:=$(BOOT_SRC_DIR)/out
+
+OBM_NTIM_1:=obm.bin.saarcnevo
+OBM_NTIM_2:=PinMuxData.bin
+OBM_NTIM_3:=obm.bin.evbnevo
+
+PRIMARY_GPT_BIN:=primary_gpt
+SECONDARY_GPT_BIN:=secondary_gpt
+PRIMARY_GPT_BIN_2:=primary_gpt_8g
+SECONDARY_GPT_BIN_2:=secondary_gpt_8g
+
+#format: <file name>:<dst folder>:[m|o]:[md5]
+#m:means mandatory
+#o:means optional
+#md5: need to generate md5 sum
+
+define define-build-uboot-obm-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY:build_uboot_obm_$$(product)
+build_uboot_obm_$$(product): private_product:=$$(product)
+build_uboot_obm_$$(product): private_device:=$$(device)
+build_uboot_obm_$$(product): build_telephony_$$(product)
+	$(log) "[$$(private_product])starting to build uboot and obm"
+	$(hide)cd $(SRC_DIR) && \
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	cd $(SRC_DIR)/$(BOOT_SRC_DIR) && \
+	make all
+	$(hide)mkdir -p $(OUTPUT_DIR)/$$(private_product)
+
+	$(log) "start to copy uboot and obm files"
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/u-boot.bin $(OUTPUT_DIR)/$$(private_product)
+	$(hide)if [ -e $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_1) ]; then cp $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_1) $(OUTPUT_DIR)/$$(private_product); fi
+	$(hide)if [ -e $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_2) ]; then cp $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_2) $(OUTPUT_DIR)/$$(private_product); fi
+	$(hide)if [ -e $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_3) ]; then cp $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_3) $(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(log) "  done."
+PUBLISHING_FILES+=$$(product)/u-boot.bin:m:md5
+PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN):m:md5
+PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN):m:md5
+PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN_2):m:md5
+PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN_2):m:md5
+PUBLISHING_FILES+=$$(product)/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES+=$$(product)/$(OBM_NTIM_2):m:md5
+PUBLISHING_FILES+=$$(product)/$(OBM_NTIM_3):m:md5
+
+endef
+
+define define-clean-uboot-obm-target
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+.PHONY:clean_uboot_obm_$$(product)
+clean_uboot_obm_$$(product): private_product:=$$(product)
+clean_uboot_obm_$$(product): private_device:=$$(device)
+clean_uboot_obm_$$(product):
+	$(log) "cleaning uboot and obm..."
+	source ./build/envsetup.sh && \
+	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
+	cd $(SRC_DIR)/$(BOOT_SRC_DIR) && \
+	#make clean
+	make clean_uboot
+	$(log) "    done."
+endef
+
+$(foreach bv, $(ABS_BUILD_DEVICES), $(eval $(call define-build-uboot-obm-target,$(bv)) ))
+$(foreach bv, $(ABS_BUILD_DEVICES), $(eval $(call define-clean-uboot-obm-target,$(bv)) ))
+
+
