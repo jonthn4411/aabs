031d3e49767e771ce57e73f7a2fb5a49e935dc63
Horace Hsieh <xiehong@marvell.com>
2012-04-16 23:02:50 +0800

    mmp2: update build-brownstone-xxx.sh for new arch
    Replace $ABS_PRODUCT_NAME with $ABS_SOC
    
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-brownstone-gingerbread.sh
+++ build-brownstone-gingerbread.sh
@@ -1,6 +1,6 @@
 export ABS_BOARD=brownstone
 export ABS_DROID_BRANCH=gingerbread
-export ABS_PRODUCT_NAME=MMP2
+export ABS_SOC=mmp2
 export ABS_DROID_VARIANT=user
 export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 export ABS_UNIQUE_MANIFEST_BRANCH=1
