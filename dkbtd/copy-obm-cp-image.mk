b661f4885b3f40128c572f2c1eb0ff250573745d
Carton He <carton.he@marvell.com>
2012-04-20 09:18:48 +0800

    Created rls_dkbtd_gingerbread_beta5sp6_zte_hs
    Branched from rls_dkbtd_gingerbread_beta5, and copied the PXA920HS
    related changes.
    
    Signed-off-by: Carton He <carton.he@marvell.com>

--- dkbtd/copy-obm-cp-image.mk
+++ dkbtd/copy-obm-cp-image.mk
@@ -3,12 +3,10 @@ $(call check-variables,BUILD_VARIANTS)
 TEL_SRC_DIR:=kernel/out/telephony/
 
 DKBTD_UBOOT:=u-boot.bin
-DKBTD_CP1:=Arbel_DKB_SKWS.bin
-DKBTD_CP2:=Arbel_DKB_SKWS_DIAG.mdb
-DKBTD_CP3:=Arbel_DKB_SKWS_NVM.mdb
-DKBTD_CP4:=TTD_M06_AI_A0_Flash.bin
-DKBTD_CP5:=TTD_M06_AI_A1_Flash.bin
-DKBTD_CP6:=pxa_symbols.tgz
+DKBTD_CP1:=Arbel_DIGRF3.bin
+DKBTD_CP2:=Arbel_DIGRF3_DIAG.mdb
+DKBTD_CP3:=Arbel_DIGRF3_NVM.mdb
+DKBTD_CP4:=920S_M07_AI_A3_Flash.bin
 DKBTD_IMEI:=ReliableData.bin
 DKBTD_SWD:=Software_Downloader.zip
 #DKBTD_NO_GUI_RAMDISK:=ramdisk_no_gui.img
@@ -26,8 +24,6 @@ PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP1):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP2):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP3):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP4):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP5):m:md5
-PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_CP6):o:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_IMEI):m:md5
 PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_SWD):m:md5
 #PUBLISHING_FILES_$(1)+=$(1)/$(DKBTD_NO_GUI_RAMDISK):m:md5
@@ -41,8 +37,6 @@ copy_obm_cp_image_$(1):
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_CP2) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_CP3) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_CP4) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_CP5) $$(OUTPUT_DIR)/$(1)
-	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_CP6) $$(OUTPUT_DIR)/$(1)
 	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_IMEI) $$(OUTPUT_DIR)/$(1)
 #	$$(hide)cp $$(SRC_DIR)/$$(TEL_SRC_DIR)/$(DKBTD_NO_GUI_RAMDISK) $$(OUTPUT_DIR)/$(1)
 	$$(log) "cp OBM and CP images  done."
