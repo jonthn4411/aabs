9c623ccd2429d4b3c86b15fb4cb87daf48241f69
Jason Ji <jcji@marvell.com>
2012-04-23 16:21:20 +0800

    mk2: copy more binaries related to tim and dtim
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- mk2/bootloader.mk
+++ mk2/bootloader.mk
@@ -7,6 +7,20 @@ BOOT_OUT_NOR_DIR:=$(BOOT_OUT_DIR)/NORMAL
 PUBLISHING_FILES+=prebuilt/emmc/u-boot.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/ntim.bin.tgz:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_primary.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_backup.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_primary.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_3_3_2_NTZ.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_3_3_2_NTZ_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/dtim_primary.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/dtim_recovery.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/dtim_platform_primary.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/dtim_platform_recovery.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/u-boot_recovery.bin:m:md5
+
 PUBLISHING_FILES+=prebuilt/emmc_cm/u-boot.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc_cm/Wtm_rel_mmp3.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc_cm/coremorphall.bin:m:md5
@@ -30,6 +44,14 @@ bootloader:
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_NOR_DIR)/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp -rf $(SRC_DIR)/$(BOOT_OUT_NOR_DIR)/ntim/ $(OUTPUT_DIR)/prebuilt/emmc/
 	$(hide)cd $(OUTPUT_DIR)/prebuilt/emmc/ && tar zcvf ntim.bin.tgz ntim
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_*.txt $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_platform_*.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/u-boot_recovery.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/u-boot.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/coremorphall.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
