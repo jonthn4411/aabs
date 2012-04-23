d7b4f498418893d66ff4cfbed6bf3c4055ecc2a2
Jason Ji <jcji@marvell.com>
2012-04-23 13:14:27 +0800

    mk2: build update packages
    Signed-off-by: Jason Ji <jcji@marvell.com>

---
+++ mk2/droidupdate.mk
@@ -0,0 +1,18 @@
+DROID_OUT:=$(SRC_DIR)/out/target/product/$(ABS_DROID_PRODUCT)
+.PHONY: droidupdate
+droidupdate:
+	$(log) "DROID UPDATE: Starting to build..."
+	$(hide)cd $(SRC_DIR) && \
+		source ./build/envsetup.sh && \
+		chooseproduct $(ABS_DROID_PRODUCT) && choosetype $(ABS_DROID_TYPE) && choosevariant $(ABS_DROID_VARIANT) && \
+		make droidupdate -j$(MAKE_JOBS)
+	$(log) "DROID UPDATE: Copying output files..."
+	$(hide)cp -p $(DROID_OUT)/update_droid.zip    $(OUTPUT_DIR)/prebuilt/
+	$(hide)cp -p $(DROID_OUT)/update_recovery.zip $(OUTPUT_DIR)/prebuilt/
+	$(log) "DROID UPDATE: Done:)"
+
+PUBLISHING_FILES+=prebuilt/update_droid.zip:m:md5
+PUBLISHING_FILES+=prebuilt/update_recovery.zip:m:md5
+
+build_device: droidupdate
+
