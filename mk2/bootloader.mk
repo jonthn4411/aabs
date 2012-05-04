062ab814b88b2b98172aded5ac33d3f569447a9c
Pengcheng Guo <pcguo@marvell.com>
2012-05-04 15:41:19 +0800

    mk2: enable both 1G and 1.2G tim support
    Change-Id: I292798fce8b11d2338ef14cc0a263a63f8b78286
    Signed-off-by: Pengcheng Guo <pcguo@marvell.com>

--- mk2/bootloader.mk
+++ mk2/bootloader.mk
@@ -6,11 +6,14 @@ BOOT_OUT_NOR_DIR:=$(BOOT_OUT_DIR)/NORMAL
 
 PUBLISHING_FILES+=prebuilt/emmc/u-boot.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/ntim.bin.tgz:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_primary.txt:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_backup.txt:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_primary.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_primary.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_backup.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_primary.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_backup.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_primary.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_primary.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3_backup.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_3_3_2_NTZ.bin:m:md5
@@ -23,8 +26,6 @@ PUBLISHING_FILES+=prebuilt/emmc/u-boot_recovery.bin:m:md5
 
 PUBLISHING_FILES+=prebuilt/emmc_cm/u-boot.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc_cm/Wtm_rel_mmp3.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc_cm/coremorphall.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc_cm/ntim.bin.tgz:m:md5
 
 .PHONY:bootloader
 bootloader:
@@ -35,28 +36,19 @@ bootloader:
 		cd $(SRC_DIR)/$(BOOT_SRC_DIR) && make all
 	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt
 	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc/ntim
-	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc_cm
-	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc_cm/ntim
 
 	$(log) "start to copy uboot and obm files"
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_NOR_DIR)/u-boot.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_NOR_DIR)/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp -rf $(SRC_DIR)/$(BOOT_OUT_NOR_DIR)/ntim/ $(OUTPUT_DIR)/prebuilt/emmc/
-	$(hide)cd $(OUTPUT_DIR)/prebuilt/emmc/ && tar zcvf ntim.bin.tgz ntim
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_*.txt $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_platform_*.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/u-boot_recovery.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-797-797-399-399-399-399-399-200__3_1_sm_400_1200mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/u-boot.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/coremorphall.bin $(OUTPUT_DIR)/prebuilt/emmc_cm
-	$(hide)cp -rf $(SRC_DIR)/$(BOOT_OUT_CM_DIR)/ntim/ $(OUTPUT_DIR)/prebuilt/emmc_cm/
-	$(hide)cd $(OUTPUT_DIR)/prebuilt/emmc_cm/ && tar zcvf ntim.bin.tgz ntim
 	$(log) "[BOOTLOADER]Done:)"
 
 .PHONY:clean_bootloader
