2f928d43a4c49d9546bfb78bf79a22a047313d84
Johnny Xia <bxia1@marvell.com>
2012-05-21 16:12:22 +0800

    brownstone: remove manifest-branch variable
    Signed-off-by: Johnny Xia <bxia1@marvell.com>

--- build-brownstone-ics.sh
+++ build-brownstone-ics.sh
@@ -2,6 +2,5 @@ export ABS_BOARD=brownstone
 export ABS_DROID_BRANCH=ics
 export ABS_PRODUCT_NAME=MMP2
 export ABS_DROID_VARIANT=userdebug
-export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 
 core/autobuild.sh $*
