dc2ecab0b2f710683fd71c2e81b330c6792af545
Carton He <carton.he@marvell.com>
2012-06-04 14:39:50 +0800

    Fix "autobuild can't email on build failure" issue
    Last commit to core/autobuild.sh forgot to remove some code so that
    action of sending email "&&" with other build actions. The sending email
    action cannot be reached if any error happens during the build process.
    
    Signed-off-by: Carton He <carton.he@marvell.com>

--- core/autobuild.sh
+++ core/autobuild.sh
@@ -404,8 +404,7 @@ if [ "$FLAG_PUBLISH" = "true" ]; then
 	echo "Project:$ABS_SOC" > $LAST_BUILD &&
 	echo "Build-Num:$BUILD_NUM" >> $LAST_BUILD &&
 	echo "Package:$PUBLISH_DIR" >> $LAST_BUILD 
-fi &&
-
+fi
 
 if [ $? -ne 0 ]; then #auto build fail, send an email
 	echo "error encountered!" 2>&1 | tee -a $STD_LOG
