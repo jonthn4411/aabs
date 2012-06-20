9125e585eea3ba17e76c9a649f3df48f0e3110c3
Jason Ji <jcji@marvell.com>
2012-06-20 18:22:06 +0800

    add tim and tim-backup update packages
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- mk2/droidupdate.mk
+++ mk2/droidupdate.mk
@@ -9,10 +9,14 @@ droidupdate:
 	$(log) "DROID UPDATE: Copying output files..."
 	$(hide)cp -p $(DROID_OUT)/update_droid.zip    $(OUTPUT_DIR)/prebuilt/
 	$(hide)cp -p $(DROID_OUT)/update_recovery.zip $(OUTPUT_DIR)/prebuilt/
+	$(hide)cp -p $(DROID_OUT)/update_tim.zip    $(OUTPUT_DIR)/prebuilt/
+	$(hide)cp -p $(DROID_OUT)/update_tim-backup.zip    $(OUTPUT_DIR)/prebuilt/
 	$(log) "DROID UPDATE: Done:)"
 
 PUBLISHING_FILES+=prebuilt/update_droid.zip:m:md5
 PUBLISHING_FILES+=prebuilt/update_recovery.zip:m:md5
+PUBLISHING_FILES+=prebuilt/update_tim.zip:m:md5
+PUBLISHING_FILES+=prebuilt/update_tim-backup.zip:m:md5
 
 build_device: droidupdate
 
