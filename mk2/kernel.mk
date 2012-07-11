8d81b3c4dd20c85c89ddec0cf6cd12bcdce11d15
Jason Ji <jcji@marvell.com>
2012-07-11 15:12:17 +0800

    MK2 JellyBean: remove all unnecessary outputs
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- mk2/kernel.mk
+++ mk2/kernel.mk
@@ -10,26 +10,14 @@ kernel:
 	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt
 	$(hide)cp $(SRC_DIR)/kernel/out/uImage.smp          $(OUTPUT_DIR)/prebuilt/uImage.smp
 	$(hide)cp $(SRC_DIR)/kernel/out/uImage_recovery.smp $(OUTPUT_DIR)/prebuilt/uImage_recovery.smp
-	$(hide)cp $(SRC_DIR)/kernel/out/uImage.cm           $(OUTPUT_DIR)/prebuilt/uImage.cm
-	$(hide)cp $(SRC_DIR)/kernel/out/uImage_recovery.cm  $(OUTPUT_DIR)/prebuilt/uImage_recovery.cm
-	$(hide)cp $(SRC_DIR)/kernel/out/rdinit              $(OUTPUT_DIR)/prebuilt
-	$(hide)cp $(SRC_DIR)/kernel/rdroot/rdroot.tgz       $(OUTPUT_DIR)/prebuilt
 	$(hide)cp $(SRC_DIR)/kernel/kernel/vmlinux          $(OUTPUT_DIR)/prebuilt
 	$(hide)cp $(SRC_DIR)/kernel/kernel/System.map       $(OUTPUT_DIR)/prebuilt
-	$(hide)rm -fr $(OUTPUT_DIR)/prebuilt/modules
-	$(hide)cp -r $(SRC_DIR)/kernel/out/modules $(OUTPUT_DIR)/prebuilt/modules
-	$(hide)cd $(OUTPUT_DIR)/prebuilt && tar czf modules.tgz modules/ 
 	$(log) "KERNEL: Done:)"
 
 PUBLISHING_FILES+=prebuilt/uImage.smp:m:md5
 PUBLISHING_FILES+=prebuilt/uImage_recovery.smp:m:md5
-PUBLISHING_FILES+=prebuilt/uImage.cm:m:md5
-PUBLISHING_FILES+=prebuilt/uImage_recovery.cm:m:md5
-PUBLISHING_FILES+=prebuilt/rdinit:m:md5
-PUBLISHING_FILES+=prebuilt/rdroot.tgz:m:md5
 PUBLISHING_FILES+=prebuilt/vmlinux:o:md5
 PUBLISHING_FILES+=prebuilt/System.map:o:md5
-PUBLISHING_FILES+=prebuilt/modules.tgz:m:md5
 
 #fixme(jason):all clean targets not verifeid yet
 .PHONEY: clean_kernel
