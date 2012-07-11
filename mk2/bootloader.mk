ef3fa768836b6b0b4ca5e079e1507e378f146a12
Jason Ji <jcji@marvell.com>
2012-07-11 17:55:13 +0800

    MK2: tim, dtim & obm updated for B0P
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- mk2/bootloader.mk
+++ mk2/bootloader.mk
@@ -13,15 +13,65 @@ PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_s
 PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_primary.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_backup.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_primary.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/Wtm_rel_mmp3_backup.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_3_3_2_NTZ.bin:m:md5
-PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_3_3_2_NTZ_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_NTZ.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/MMP3_LINUX_ARM_NTZ_backup.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/dtim_primary.txt:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/dtim_recovery.txt:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/dtim_platform_primary.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/dtim_platform_recovery.bin:m:md5
 PUBLISHING_FILES+=prebuilt/emmc/u-boot_recovery.bin:m:md5
+
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/Wtm_rel_mmp3_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/MMP3_LINUX_ARM_NTZ_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/EB_JO/MMP3_LINUX_ARM_NTZ.bin:m:md5
+
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/Wtm_rel_mmp3_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/MMP3_LINUX_ARM_NTZ_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB_JO/MMP3_LINUX_ARM_NTZ.bin:m:md5
+
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_backup_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_primary_b0p.txt:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/Wtm_rel_mmp3_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/Wtm_rel_mmp3.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/MMP3_LINUX_ARM_NTZ_backup.bin:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PB/MMP3_LINUX_ARM_NTZ.bin:m:md5
+
+PUBLISHING_FILES+=prebuilt/emmc/print_MMP3_FuseVal.xdb:m:md5
+PUBLISHING_FILES+=prebuilt/emmc/PRE_SetupClocks.xdb:m:md5
 
 .PHONY:bootloader
 bootloader:
@@ -38,12 +88,38 @@ bootloader:
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_*.txt $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/dtim_platform_*.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/u-boot_recovery.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_noch2_400_1250mv_*.*    $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_noch2_400_1325mv_*.* $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1063-1063-532-532-399-399-399-200__4_1_sm_400_1275mv_*_b0p.*      $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/tim_mk2-1196-1196-598-598-399-399-399-200__1p2g_1_sm_400_1275mv_*_b0p.*   $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3.bin $(OUTPUT_DIR)/prebuilt/emmc
 	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/Wtm_rel_mmp3_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ.bin $(OUTPUT_DIR)/prebuilt/emmc
-	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_3_3_2_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_NTZ.bin $(OUTPUT_DIR)/prebuilt/emmc
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_dtim/MMP3_LINUX_ARM_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc
+
+	$(hide)cp $(SRC_DIR)/device/marvell/mk2/development/* $(OUTPUT_DIR)/prebuilt/emmc/
+
+	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/EB_JO/tim_mk2-*.*                   $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/EB_JO/MMP3_LINUX_ARM_NTZ.bin        $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/EB_JO/MMP3_LINUX_ARM_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/EB_JO/Wtm_rel_mmp3_backup.bin       $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/EB_JO/Wtm_rel_mmp3.bin              $(OUTPUT_DIR)/prebuilt/emmc/EB_JO
+
+	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB_JO/tim_mk2-*.*                   $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB_JO/MMP3_LINUX_ARM_NTZ.bin        $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB_JO/MMP3_LINUX_ARM_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB_JO/Wtm_rel_mmp3_backup.bin       $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB_JO/Wtm_rel_mmp3.bin              $(OUTPUT_DIR)/prebuilt/emmc/PB_JO
+
+	$(hide)mkdir -p $(OUTPUT_DIR)/prebuilt/emmc/PB
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB/tim_mk2-*.*                   $(OUTPUT_DIR)/prebuilt/emmc/PB
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB/MMP3_LINUX_ARM_NTZ.bin        $(OUTPUT_DIR)/prebuilt/emmc/PB
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB/MMP3_LINUX_ARM_NTZ_backup.bin $(OUTPUT_DIR)/prebuilt/emmc/PB
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB/Wtm_rel_mmp3_backup.bin       $(OUTPUT_DIR)/prebuilt/emmc/PB
+	$(hide)cp $(SRC_DIR)/$(BOOT_OUT_DIR)/tim_product/PB/Wtm_rel_mmp3.bin              $(OUTPUT_DIR)/prebuilt/emmc/PB
+
 	$(log) "[BOOTLOADER]Done:)"
 
 .PHONY:clean_bootloader
