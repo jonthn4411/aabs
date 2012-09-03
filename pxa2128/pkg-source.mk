d081b3879435528ad481ae08775f894d45b80f9c
Jason Ji <jcji@marvell.com>
2012-09-03 16:09:28 +0800

    PXA2128: refine pkg-source.mk
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- pxa2128/pkg-source.mk
+++ pxa2128/pkg-source.mk
@@ -1,13 +1,3 @@
-include core/pkg-source.mk
-
-EXCLUDE_VCS=--exclude-vcs --exclude=.repo
-
-DROID_BASE:=android-4.1.1_r1
-KERNEL_BASE_COMMIT:=5e4fcd2c556e25e1b6787dcd0c97b06e29e42292
-UBOOT_BASE_COMMIT:=b20a91d81fbdf9402df5425126bdee3368f10044
-
-HEAD_MANIFEST:=head_manifest.default
-
 .PHONY:pkgsrc
 pkgsrc:
 	$(log) Marvell will never need this target soon!
