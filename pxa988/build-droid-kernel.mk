6f6751fe6ef3d5232155b945c04bffa975690e35
Weichuan Yan <wchyan@marvell.com>
2012-09-19 18:35:28 +0800

    pxa988: publish new CP images
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -164,6 +164,15 @@ PUBLISHING_FILES+=$$(product)/pxafs.img:o:md5
 PUBLISHING_FILES+=$$(product)/pxa_symbols.tgz:o:md5
 PUBLISHING_FILES+=$$(product)/radio.img:o:md5
 
+PUBLISHING_FILES+=$$(product)/EM_CP.bin:o:md5
+PUBLISHING_FILES+=$$(product)/EM_CP_DIAG.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/EM_CP_NVM.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/EM_M08_AI_Z1_Flash.bin:o:md5
+PUBLISHING_FILES+=$$(product)/KL_CP.bin:o:md5
+PUBLISHING_FILES+=$$(product)/KL_CP_DIAG.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/KL_CP_NVM.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/KUNLUN_Z0_M14_AI_Flash.bin:o:md5
+
 PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel.bin:o:md5
 PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel_DIAG.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel_NVM.mdb:o:md5
