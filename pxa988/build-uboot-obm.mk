5b49f47dd2ed94d8bf1edcacab10bbecaf0ef23d
Horace Hsieh <xiehong@marvell.com>
2012-12-18 16:58:45 +0800

    pxa988ff: publish 4 files
    out/target/product/pxa988ff/uboot-obm/EMEI_DKB/EMEI_NonTLoader_eMMC_ARM_SRAM_3_3_2.bin
    out/target/product/pxa988ff/primary_gpt_4g
    out/target/product/pxa988ff/secondary_gpt_4g
    out/target/product/pxa988ff/uboot-obm/EMEI_DKB/988FF_Nontrusted_eMMC_4G.blf
    
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- pxa988/build-uboot-obm.mk
+++ pxa988/build-uboot-obm.mk
@@ -7,14 +7,17 @@ DROID_OUT:=out/target/product
 #OBM_NTLOADER_1:=ASPN_NTLOADER_avengers-a_slc.bin
 #OBM_NTLOADER_2:=ASPN_NTLOADER_spi.bin
 
-#OBM_NTIM_1:=NEVO_Loader_eMMC_ARM_3_3_1.bin
-#OBM_NTIM_1:=TAVOR_SAAR_NTOBM_EMMC_MODE1.bin.rnd
+OBM_BLF:=988FF_Nontrusted_eMMC_4G.blf
+
+OBM_NTIM_1:=EMEI_NonTLoader_eMMC_ARM_SRAM_3_3_2.bin
 #OBM_NTIM_2:=ntim_a0_avengers-a_1.6F_256mb_400mhz_mode3_pm_spi.bin
 
 PRIMARY_GPT_BIN:=primary_gpt
 SECONDARY_GPT_BIN:=secondary_gpt
-#PRIMARY_GPT_BIN_2:=primary_gpt_8g
-#SECONDARY_GPT_BIN_2:=secondary_gpt_8g
+PRIMARY_GPT_BIN_2:=primary_gpt_8g
+SECONDARY_GPT_BIN_2:=secondary_gpt_8g
+PRIMARY_GPT_BIN_3:=primary_gpt_4g
+SECONDARY_GPT_BIN_3:=secondary_gpt_4g
 
 PRODUCT_OUT:=common
 #format: <file name>:<dst folder>:[m|o]:[md5]
@@ -41,19 +44,25 @@ build_uboot_obm_$$(product):
 	$(log) "start to copy uboot and obm files"
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/uboot-obm/u-boot.bin $(OUTPUT_DIR)/$$(private_product)
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/uboot-obm/Software_Downloader.zip $(OUTPUT_DIR)/$$(private_product)
-	#$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$(OBM_NTIM_1) ]; then cp $(SRC_DIR)/$(BOOT_OUT_DIR)/$(OBM_NTIM_1) $(OUTPUT_DIR)/$$(private_product); fi
-	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
-	$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
-	#$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(PRIMARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
-	#$$(hide)if [ -e $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/$$(SECONDARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
+	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/uboot-obm/EMEI_DKB/$(OBM_NTIM_1) ]; then cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/uboot-obm/EMEI_DKB/$(OBM_NTIM_1) $(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN_2) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN_2) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN_3) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(PRIMARY_GPT_BIN_3) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN_3) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(SECONDARY_GPT_BIN_3) $$(OUTPUT_DIR)/$$(private_product); fi
+	$$(hide)if [ -e $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(OBM_BLF) ]; then cp $$(SRC_DIR)/$(DROID_OUT)/$$(private_device)/$$(OBM_BLF) $$(OUTPUT_DIR)/$$(private_product); fi
 	$$(log) "  done."
 PUBLISHING_FILES+=$$(product)/u-boot.bin:m:md5
 PUBLISHING_FILES+=$$(product)/Software_Downloader.zip:m:md5
 PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN):o:md5
 PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN):o:md5
-#PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN_2):m:md5
-#PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN_2):m:md5
-#PUBLISHING_FILES+=$$(product)/$(OBM_NTIM_1):m:md5
+PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN_2):o:md5
+PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN_2):o:md5
+PUBLISHING_FILES+=$$(product)/$(PRIMARY_GPT_BIN_3):o:md5
+PUBLISHING_FILES+=$$(product)/$(SECONDARY_GPT_BIN_3):o:md5
+PUBLISHING_FILES+=$$(product)/$(OBM_NTIM_1):o:md5
+PUBLISHING_FILES+=$$(product)/$(OBM_BLF):o:md5
 
 endef
 
