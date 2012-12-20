ef9d8dcf515a756454dd44a6dc802e41e5704625
Horace Hsieh <xiehong@marvell.com>
2012-12-20 11:27:55 +0800

    pxa988ff-t7p: remove the hardcoded manifest branch
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-pxa988ff-jb.sh
+++ build-pxa988ff-jb.sh
@@ -2,14 +2,10 @@ export ABS_SOC=pxa988
 export ABS_DROID_BRANCH=jb
 export ABS_DROID_VARIANT=userdebug
 export ABS_BOARD=pxa988ff
-export ABS_MANIFEST_BRANCH="rls_pxa988_jb_wfdff"
-#export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 if [ -z "$ABS_BUILDHOST_DEF" ]; then
     ABS_BUILDHOST_DEF=buildhost4.def
 fi
 export ABS_BUILDHOST_DEF
-
-export ABS_UNIQUE_MANIFEST_BRANCH=1
 
 core/autobuild.sh $*
