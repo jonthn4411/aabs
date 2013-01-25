ccd074d7611edb876e64c2f3fbe7cd2bd89b66cc
Jia Guo <guojia@marvell.com>
2013-01-25 14:57:58 +0800

    pxa988: remove tools.img
    Signed-off-by: Jia Guo <guojia@marvell.com>
    
    Conflicts:
    	pxa988/build-droid-kernel.mk

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -302,11 +302,3 @@ $(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call define-build-droid-kernel-targe
 				$(eval $(call define-build-droid-otapackage,$(bv)) ) \
 				$(eval $(call define-build-droid-security,$(bv)) ) \
 )
-#$(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call define-build-droid-kernel-target,$(bv)) )\
-				$(eval $(call define-build-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-target,$(bv)) ) \
-				$(eval $(call define-clean-droid-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-otapackage,$(bv)) ) \
-				$(eval $(call define-build-droid-tool,$(bv)) ) \
-				$(eval $(call define-build-droid-security,$(bv)) ) \
-)
