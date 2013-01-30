b434a05aaaa298543d475fc5667f09fcec944cb7
Weichuan Yan <wchyan@marvell.com>
2013-01-30 18:51:46 +0800

    pxa1088: add helan cp images
    Signed-off-by: Weichuan Yan <wchyan@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -174,6 +174,12 @@ PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel.bin:o:md5
 PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel_DIAG.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/KUNLUN_Arbel_NVM.mdb:o:md5
 
+PUBLISHING_FILES+=$$(product)/HL_TD_CP.bin:o:md5
+PUBLISHING_FILES+=$$(product)/HL_TD_CP_DIAG.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/HL_TD_CP_NVM.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/HL_TD_M08_AI_A0_Flash.bin:o:md5
+PUBLISHING_FILES+=$$(product)/radio-helan-td.img:o:md5
+
 PUBLISHING_FILES+=$$(product)/WK_CP_2CHIP_SPRW_NVM.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/WK_CP_2CHIP_SPRW_DIAG.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/Boerne_DIAG.mdb.txt:o:md5
