091cc5884593ce91d6ce5310b1cbe944fd3f336a
Horace Hsieh <xiehong@marvell.com>
2013-02-01 15:58:04 +0800

    pxa988ff_jb42: support jb42_wfdff
    Change-Id: I750933b7af7d3d94ea92bfc75d08a9cc66cbbe54
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-pxa988-jb4.2.sh
+++ build-pxa988-jb4.2.sh
@@ -1,12 +1,1 @@
-export ABS_SOC=pxa988
-export ABS_DROID_BRANCH=jb4.2
-export ABS_DROID_VARIANT=userdebug
-export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
-if [ -z "ABS_BUILDHOST_DEF" ]; then
-    ABS_BUILDHOST_DEF=buildhost.def
-fi
-export ABS_BUILDHOST_DEF
-
-export ABS_UNIQUE_MANIFEST_BRANCH=1
-
-core/autobuild.sh $*
+build-pxa988ff-jb4.2.sh
