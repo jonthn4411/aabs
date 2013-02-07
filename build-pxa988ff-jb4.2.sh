e381ab7175391098a38d74b69af8448e308bc1f4
Horace Hsieh <xiehong@marvell.com>
2013-02-07 14:24:58 +0800

    pxa988t7p_jb42: change branch of manifest to rls_pxa988_jb4.2_wfdt7p
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- build-pxa988ff-jb4.2.sh
+++ build-pxa988ff-jb4.2.sh
@@ -2,7 +2,7 @@ export ABS_SOC=pxa988
 export ABS_DROID_BRANCH=jb4.2
 export ABS_DROID_VARIANT=userdebug
 export ABS_BOARD=pxa988ff
-export ABS_MANIFEST_BRANCH="rls_pxa988_jb4.2_wfdff"
+export ABS_MANIFEST_BRANCH="rls_pxa988_jb4.2_wfdt7p"
 #export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 if [ -z "$ABS_BUILDHOST_DEF" ]; then
