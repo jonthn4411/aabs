34fb34852429a46ecbbe754a41be84a95a4f382d
Jason Ji <jcji@marvell.com>
2013-03-15 17:06:05 +0800

    saarpv: removed from mainstream
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-saarpv-eclair.sh
+++
@@ -1,6 +0,0 @@
-export ABS_BOARD=saarpv
-export ABS_DROID_BRANCH=eclair
-export ABS_PRODUCT_NAME=saar
-export PATH=/usr/lib/jvm/java-1.5.0-sun/bin:$PATH
-
-core/autobuild.sh $*
--- saarpv-eclair.mk
+++
@@ -1,5 +0,0 @@
-BOARD:=saarpv
-ANDROID_VERSION:=eclair
-PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
-
-include saarpv/board.mk
