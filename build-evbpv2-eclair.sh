4527bca739035b63e0535e6e593a079f417262f8
Jason Ji <jcji@marvell.com>
2013-03-15 17:12:47 +0800

    evbpv2: removed from main stream
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-evbpv2-eclair.sh
+++
@@ -1,5 +0,0 @@
-export ABS_BOARD=evbpv2
-export ABS_DROID_BRANCH=eclair
-export ABS_PRODUCT_NAME=evbpv2
-
-core/autobuild.sh $*
--- evbpv2-eclair.mk
+++
@@ -1,5 +0,0 @@
-BOARD:=evbpv2
-ANDROID_VERSION:=eclair
-PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
-
-include evbpv2/board.mk
