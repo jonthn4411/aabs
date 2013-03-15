4ab5bda1b3ec2cb0b311a34c4943158e3587e37e
Jason Ji <jcji@marvell.com>
2013-03-15 17:09:52 +0800

    g50: removed from mainstream
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- build-g50-gingerbread.sh
+++
@@ -1,7 +0,0 @@
-export ABS_BOARD=brownstone
-export ABS_DROID_BRANCH=gingerbread
-export ABS_PRODUCT_NAME=MMP2
-export ABS_DROID_VARIANT=user
-export ABS_MANIFEST_BRANCH="rls_g50_gingerbread_beta1"
-
-core/autobuild.sh $*
