7ad387ce21cad1e48bdf20777e8dfae5edcd8ac4
Jason Ji <jcji@marvell.com>
2013-04-12 14:19:29 +0800

    Core: add common part of pkg-source
    1. platforms have copy-paste code in pkg-source.mk
       In this patch, we add code determining:
       HEAD_MANIFEST, KERNEL_BASE_COMMIT, UBOOT_BASE_COMMIT, DROID_BASE
    2. eden and pxa988's common part is removed in this patch
    
    Signed-off-by: Jason Ji <jcji@marvell.com>

--- core/pkg-source.mk
+++ core/pkg-source.mk
@@ -21,6 +21,78 @@ get_source_for_pkg: output_dir
 	repo init -m autobuild.xml && \
 	repo sync -l 
 
+EXCLUDE_VCS=--exclude-vcs --exclude=.repo
+
+KERNEL_2_6_32_BASE_COMMIT:=f6320db51173e3b94f54b87944b88d3b363c4487
+KERNEL_2_6_35_BASE_COMMIT:=6631e089ef3dc4be98e78aafaca11047e7edf193
+KERNEL_3_0_BASE_COMMIT:= 5e4fcd2c556e25e1b6787dcd0c97b06e29e42292
+KERNEL_3_4_BASE_COMMIT:= a636fc98b8e21a2360186eec9871337a87142758
+
+UBOOT_2009RC1_BASE_COMMIT:=aced78d852d0b009e8aaa1445af8cb40861ee549
+UBOOT_201009_BASE_COMMIT:=1a2d9b30e31e2b7ed0acb64bfb2290911e3c9efb
+UBOOT_201109_BASE_COMMIT:=1d5e7fb403257d62f0f2419cb83fdf6b0f02f215
+
+HEAD_MANIFEST:=head_manifest.default
+KERNEL_BASE_COMMIT:=$(KERNEL_2_6_32_BASE_COMMIT)
+UBOOT_BASE_COMMIT:=$(UBOOT_2009RC1_BASE_COMMIT)
+
+ifeq ($(ABS_DROID_BRANCH),donut)
+	DROID_BASE:=shgit/donut-release
+else
+ifeq ($(ABS_DROID_BRANCH),eclair)
+	DROID_BASE:=android-2.1_r2
+else
+ifeq ($(ABS_DROID_BRANCH),froyo)
+	DROID_BASE:=android-2.2.2_r1
+else
+ifeq ($(ABS_DROID_BRANCH),gingerbread)
+	KERNEL_BASE_COMMIT:=$(KERNEL_2_6_35_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201009_BASE_COMMIT)
+	DROID_BASE:=android-2.3.7_r1
+else
+ifeq ($(ABS_DROID_BRANCH),honeycomb)
+	KERNEL_BASE_COMMIT:=$(KERNEL_2_6_35_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201009_BASE_COMMIT)
+	DROID_BASE:=shgit/honeycomb-mr2-release
+	HEAD_MANIFEST:=head_manifest.hc
+else
+ifeq ($(ABS_DROID_BRANCH),ics)
+	KERNEL_BASE_COMMIT:=$(KERNEL_3_0_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201109_BASE_COMMIT)
+	DROID_BASE:=android-4.0.4_r1.1
+else
+ifeq ($(ABS_DROID_BRANCH),jb)
+	KERNEL_BASE_COMMIT:=$(KERNEL_3_4_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201109_BASE_COMMIT)
+	DROID_BASE:=android-4.1.2_r1
+else
+ifeq ($(ABS_DROID_BRANCH),jb4.2)
+	KERNEL_BASE_COMMIT:=$(KERNEL_3_4_BASE_COMMIT)
+	UBOOT_BASE_COMMIT:=$(UBOOT_201109_BASE_COMMIT)
+	DROID_BASE:=android-4.2.1_r1
+endif
+endif
+endif
+endif
+endif
+endif
+endif
+endif
+
+#format: <file name>:[m|o]:[md5]
+#m:means mandatory
+#o:means optional
+#md5: need to generate md5 sum
+PUBLISHING_FILES2+=droid_all_src.tgz:src:o:md5
+PUBLISHING_FILES2+=android_src.tgz:src:m:md5
+PUBLISHING_FILES2+=android_patches.tgz:src:m:md5
+PUBLISHING_FILES2+=kernel_src.tgz:src:m:md5
+PUBLISHING_FILES2+=kernel_patches.tgz:src:m:md5
+PUBLISHING_FILES2+=uboot_src.tgz:src:m:md5
+PUBLISHING_FILES2+=uboot_patches.tgz:src:m:md5
+PUBLISHING_FILES2+=obm_src.tgz:src:m:md5
+PUBLISHING_FILES2+=marvell_manifest.xml:src:m
+PUBLISHING_FILES2+=setup_android.sh:src:m
 
 # Platform hook
 -include $(ABS_SOC)/pkg-source.mk
--- pxa988/pkg-source.mk
+++ pxa988/pkg-source.mk
@@ -2,58 +2,6 @@ INTERNAL_PROJECTS +=vendor/marvell/external/gps_sirf
 INTERNAL_PROJECTS +=vendor/marvell/generic/apps/CmmbPlayer
 INTERNAL_PROJECTS +=vendor/marvell/generic/apps/CmmbStack
 
-EXCLUDE_VCS=--exclude-vcs --exclude=.repo
-
-UBOOT_BASE_COMMIT:=1d5e7fb403257d62f0f2419cb83fdf6b0f02f215
-ifeq ($(ANDROID_VERSION),eclair)
-	KERNEL_BASE_COMMIT:=8e0ee43bc2c3e19db56a4adaa9a9b04ce885cd84
-else
-ifeq ($(ANDROID_VERSION),froyo)
-	KERNEL_BASE_COMMIT:=f6320db51173e3b94f54b87944b88d3b363c4487
-else
-ifeq ($(ANDROID_VERSION),gingerbread)
-	KERNEL_BASE_COMMIT:=49e8954d66ce9ccf75f951a5adb217209ae6f78f
-else
-ifeq ($(ANDROID_VERSION),honeycomb)
-	KERNEL_BASE_COMMIT:=49e8954d66ce9ccf75f951a5adb217209ae6f78f
-else
-	KERNEL_BASE_COMMIT:=a636fc98b8e21a2360186eec9871337a87142758
-endif
-endif
-endif
-endif
-
-ifeq ($(ANDROID_VERSION),donut)
-	DROID_BASE:=shgit/donut-release
-else
-ifeq ($(ANDROID_VERSION),eclair)
-	DROID_BASE:=android-2.1_r2
-else
-ifeq ($(ANDROID_VERSION),froyo)
-	DROID_BASE:=android-2.2.2_r1
-else
-ifeq ($(ANDROID_VERSION),gingerbread)
-	DROID_BASE:=android-2.3.7_r1
-else
-ifeq ($(ANDROID_VERSION),ics)
-	DROID_BASE:=android-4.0.4_r1
-else
-ifeq ($(ANDROID_VERSION),jb)
-	DROID_BASE:=android-4.1.2_r1
-else
-ifeq ($(ANDROID_VERSION),jb4.2)
-	DROID_BASE:=android-4.2.1_r1
-else
-	DROID_BASE:=shgit/honeycomb-mr2-release
-	HEAD_MANIFEST:=head_manifest.hc
-endif
-endif
-endif
-endif
-endif
-endif
-endif
-
 .PHONY:pkgsrc
 pkgsrc: output_dir get_source_for_pkg
 	$(hide)echo "  save project list"
@@ -78,19 +26,4 @@ pkgsrc: output_dir get_source_for_pkg
 	$(hide)cp $(BOARD)/release_package_list $(OUTPUT_DIR)/release_package_list
 	$(log) "  done."
 
-
-#format: <file name>:[m|o]:[md5]
-#m:means mandatory
-#o:means optional
-#md5: need to generate md5 sum
-PUBLISHING_FILES2+=droid_all_src.tgz:src:o:md5 
-PUBLISHING_FILES2+=android_src.tgz:src:m:md5 
-PUBLISHING_FILES2+=android_patches.tgz:src:m:md5 
-PUBLISHING_FILES2+=kernel_src.tgz:src:m:md5 
-PUBLISHING_FILES2+=kernel_patches.tgz:src:m:md5 
-PUBLISHING_FILES2+=uboot_src.tgz:src:m:md5 
-PUBLISHING_FILES2+=uboot_patches.tgz:src:m:md5 
-PUBLISHING_FILES2+=obm_src.tgz:src:m:md5 
-PUBLISHING_FILES2+=marvell_manifest.xml:src:m
-PUBLISHING_FILES2+=setup_android.sh:src:m
 PUBLISHING_FILES+=release_package_list:o
