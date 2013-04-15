145c1f77f0347b8c64c4f5efba3296272bd9ba17
Horace Hsieh <xiehong@marvell.com>
2013-04-15 15:27:28 +0800

    eden: add the support of jb42
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

---
+++ eden/build-eden-jb42.sh
@@ -0,0 +1,7 @@
+export ABS_BOARD=concord
+export ABS_DROID_BRANCH=jb42
+export ABS_SOC=eden
+export ABS_DROID_VARIANT=userdebug
+#export ABS_MANIFEST_BRANCH="$ABS_BOARD-$ABS_DROID_BRANCH"
+export ABS_UNIQUE_MANIFEST_BRANCH=1
+
