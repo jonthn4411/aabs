9c4b05e89d4ccb38f772c510135bc014f9bb46b4
Horace Hsieh <xiehong@marvell.com>
2013-05-02 17:39:49 +0800

    eden: publish gpt tables
    Signed-off-by: Horace Hsieh <xiehong@marvell.com>

--- eden/build-droid-kernel.mk
+++ eden/build-droid-kernel.mk
@@ -48,6 +48,9 @@ PUBLISHING_FILES+=$$(product)/userdata.img:o:md5
 PUBLISHING_FILES+=$$(product)/ramdisk.img:o:md5
 PUBLISHING_FILES+=$$(product)/cache.img:o:md5
 
+PUBLISHING_FILES+=$$(product)/primary_gpt:o:md5
+PUBLISHING_FILES+=$$(product)/secondary_gpt:o:md5
+
 .PHONY: build_droid_root_$$(product)
 build_droid_root_$$(product): private_product:=$$(product)
 build_droid_root_$$(product): private_device:=$$(device)
@@ -64,6 +67,8 @@ build_droid_root_$$(product): output_dir
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/userdata.img ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/userdata.img $$(OUTPUT_DIR)/$$(private_product)/; fi
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/ramdisk.img ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/ramdisk.img $$(OUTPUT_DIR)/$$(private_product)/; fi
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/cache.img ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/cache.img $$(OUTPUT_DIR)/$$(private_product)/; fi
+	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/primary_gpt ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/primary_gpt $$(OUTPUT_DIR)/$$(private_product)/; fi
+	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/secondary_gpt ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/secondary_gpt $$(OUTPUT_DIR)/$$(private_product)/; fi
 	echo "    generating symbols_lib.tgz..." && \
 		cp -a $$(SRC_DIR)/out/target/product/$$(private_device)/symbols/system/lib $$(OUTPUT_DIR)/$$(private_product) && \
 		cd $$(OUTPUT_DIR)/$$(private_product) && tar czf symbols_lib.tgz lib && rm lib -rf
