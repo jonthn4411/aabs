da20d4c6f83c950bef42c75b5c8c6c6b81873937
Fangfang Shi <shiff@marvell.com>
2013-07-25 11:06:57 +0800

    pxa988t7_dsds: add build script in aabs
    Signed-off-by: Fangfang Shi <shiff@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -208,6 +208,12 @@ PUBLISHING_FILES+=$$(product)/radio-helan-wt.img:o:md5
 PUBLISHING_FILES+=$$(product)/TABLET_CP.bin:o:md5
 PUBLISHING_FILES+=$$(product)/TABLET_MSA.bin:o:md5
 
+PUBLISHING_FILES+=$$(product)/KL_DSDS_CP.bin:o:md5
+PUBLISHING_FILES+=$$(product)/KL_DSDS_CP_DIAG.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/KL_DSDS_CP_NVM.mdb:o:md5
+PUBLISHING_FILES+=$$(product)/KUNLUN_A0_M15_AI_DSDS_Flash.bin:o:md5
+PUBLISHING_FILES+=$$(product)/KUNLUN_Z0_M14_AI_DSDS_Flash.bin:o:md5
+
 PUBLISHING_FILES+=$$(product)/WK_CP_2CHIP_SPRW_NVM.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/WK_CP_2CHIP_SPRW_DIAG.mdb:o:md5
 PUBLISHING_FILES+=$$(product)/Boerne_DIAG.mdb.txt:o:md5
