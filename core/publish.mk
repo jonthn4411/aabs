834d0fcd0a051944dc37207bb24c4605024fc7f3
Jia Guo <guojia@marvell.com>
2013-09-24 13:24:27 +0800

    Core: fix error in folder publish
    Signed-off-by: Jia Guo <guojia@marvell.com>

--- core/publish.mk
+++ core/publish.mk
@@ -19,7 +19,7 @@ define cpif-with-md5
 	$(if $(findstring $(strip $(3)),md5), \
 		@if [ -f $1 ]; then echo "generating md5 for $(2)"; \
 		cd $(dir $(1)) && \
-		md5sum $(notdir $(1)) >>$(OUTPUT_DIR)/$(MD5_FILE); fi\
+		md5sum $(notdir $(1)) >>$(OUTPUT_DIR)/$(MD5_FILE); fi; \
 		@if [ -d $1 ]; then echo "generating md5 for $(2)"; \
 		cd $(dir $(1)) && \
 		ls|xargs md5sum >>$(OUTPUT_DIR)/$(MD5_FILE); fi\
