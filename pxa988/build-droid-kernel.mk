68929309d3035ec126e8c4784ec3ebafba283199
Jia Guo <guojia@marvell.com>
2013-11-13 19:25:09 +0800

    pxa988: skip modules.tgz if no modules
    Signed-off-by: Jia Guo <guojia@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -116,7 +116,7 @@ build_droid_$$(product):
 	source ./build/envsetup.sh && \
 	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
 	make -j8 && \
-	tar zcf $(OUTPUT_DIR)/$$(private_product)/modules.tgz -C $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/root/lib modules && \
+	$(hide)if [ -d $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/root/lib ]; then tar zcf $(OUTPUT_DIR)/$$(private_product)/modules.tgz -C $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/root/lib modules; fi 
 	tar zcf $(OUTPUT_DIR)/$$(private_product)/symbols_system.tgz -C $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/ symbols
 
 	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/root ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/root; fi
