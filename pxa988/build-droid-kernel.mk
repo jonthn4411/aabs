44eb65e9607f2f324ff9da13805020432013d7a2
Jia Guo <guojia@marvell.com>
2013-12-17 10:27:53 +0800

    pxa988: update dts file copy
    Signed-off-by: Jia Guo <guojia@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -152,9 +152,7 @@ build_droid_$$(product):
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/obm*bin $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/uImage $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/zImage $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)cp -f $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/pxa1L88dkb.dtb $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)cp -f $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/pxa1088dkb.dtb $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)cp -f $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/pxa988dkb.dtb $(OUTPUT_DIR)/$$(private_product)/
+	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/*.dtb $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/boot.img $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/recovery.img $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/u-boot.bin $(OUTPUT_DIR)/$$(private_product)/
