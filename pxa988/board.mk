12388e1eca0e433b0f019d3a0df5681c947ca756
Fangfang Shi <shiff@marvell.com>
2014-01-09 13:57:37 +0800

    L7: add pxa1L88l7 device and remove other devices
    Signed-off-by: Fangfang Shi <shiff@marvell.com>

--- pxa988/board.mk
+++ pxa988/board.mk
@@ -1,4 +1,4 @@
-ABS_BUILD_DEVICES ?= pxa1L88dkb_def:pxa1L88dkb pxa1L88dkb_tz:pxa1L88dkb pxa1088dkb_def:pxa1088dkb pxa1088dkb_tz:pxa1088dkb
+ABS_BUILD_DEVICES ?= pxa1L88l7_def:pxa1L88l7 pxa1L88l7_tz:pxa1L88l7
 BOARD:=pxa988
 ANDROID_VERSION:=$(ABS_DROID_BRANCH)
 PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
