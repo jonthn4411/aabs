4c5135a53214e92b8ab4f516071d81dd234c60d0
Jianchun Zuo <jzuo@marvell.com>
2014-01-24 18:04:09 +0800

    eden: fix tz binaries publish failure
    Change-Id: I5907c68f91dbf8f8c725d6b8500fe7f7f13d53de
    Signed-off-by: Jianchun Zuo <jzuo@marvell.com>

--- eden/build-droid-kernel.mk
+++ eden/build-droid-kernel.mk
@@ -61,7 +61,7 @@ PUBLISHING_FILES+=$$(product)/vmlinux:o:md5
 ##!!## blf files
 PUBLISHING_FILES+=$$(product)/blf:o:md5
 PUBLISHING_FILES2+=Software_Downloader.zip:./:m:md5
-ifeq ($(product),concord_tz)
+ifeq ($(product),edenconcord_tz)
 PUBLISHING_FILES+=$$(product)/teesst.img:o:md5
 PUBLISHING_FILES+=$$(product)/tee_tw.bin:o:md5
 PUBLISHING_FILES+=$$(product)/wtm_rel_eden_RealOTP.bin:o:md5
