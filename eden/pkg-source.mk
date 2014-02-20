d27a1495a7e47f69c0d22c2412102318cf2ba1c7
Jianchun Zuo <jzuo@marvell.com>
2014-02-20 11:34:57 +0800

    a0bringup: disable pkg source to reduce build time
    Change-Id: I6d373ee8f81886c2afdbb686b54a743b44e3ed9d
    Signed-off-by: Jianchun Zuo <jzuo@marvell.com>

--- eden/pkg-source.mk
+++ eden/pkg-source.mk
@@ -1,11 +1,11 @@
 
 pkgsrc: save_prjlist
-pkgsrc: pkg_all_src
-pkgsrc: pkg_kernel_src
-pkgsrc: pkg_boot_src
-pkgsrc: pkg_droid_src
-pkgsrc: delta_patches
-pkgsrc: publish_setup_sh
+#pkgsrc: pkg_all_src
+#pkgsrc: pkg_kernel_src
+#pkgsrc: pkg_boot_src
+#pkgsrc: pkg_droid_src
+#pkgsrc: delta_patches
+#pkgsrc: publish_setup_sh
 #pkgsrc: publish_RN
 
 #
