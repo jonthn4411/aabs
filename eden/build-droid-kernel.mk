59f254b35c0c88ebf01ba971188cc4a0534d7c1f
Jianchun Zuo <jzuo@marvell.com>
2014-02-21 10:54:51 +0800

    fix script error
    Change-Id: Id561cca465a229b4cd29ca9caacabf0f8e7d8345
    Signed-off-by: Jianchun Zuo <jzuo@marvell.com>

--- eden/build-droid-kernel.mk
+++ eden/build-droid-kernel.mk
@@ -87,7 +87,7 @@ build_droid_root_$$(product): output_dir
 	$$(hide)cd $$(SRC_DIR) && \
 	. build/envsetup.sh && \
 	lunch $$(private_product)-$$(DROID_VARIANT) && \
-	make -j$$(MAKE_JOBS) && \
+	make -j$$(MAKE_JOBS)
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/security/teesst.img ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/security/teesst.img $$(OUTPUT_DIR)/$$(private_product)/; fi
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/security/tee_tw.bin ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/security/tee_tw.bin $$(OUTPUT_DIR)/$$(private_product)/; fi
 	$$(hide)if [ -f $$(SRC_DIR)/out/target/product/$$(private_device)/security/wtm_rel_eden_RealOTP.bin ]; then cp $$(SRC_DIR)/out/target/product/$$(private_device)/security/wtm_rel_eden_RealOTP.bin $$(OUTPUT_DIR)/$$(private_product)/; fi
