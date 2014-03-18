991b689f0d6396fb131a6663d862f64c52efa93e
Jia Guo <guojia@marvell.com>
2014-03-18 14:18:27 +0800

    pxa988: update wtm binary
    Signed-off-by: Jia Guo <guojia@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -157,7 +157,7 @@ build_droid_$$(product):
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/vmlinux $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/System.map $(OUTPUT_DIR)/$$(private_product)/
 	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/Software_Downloader.zip $(OUTPUT_DIR)/
-	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/LoopSelf.bin ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/LoopSelf.bin $(OUTPUT_DIR)/$$(private_product)/; fi
+	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/WTM.bin ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/WTM.bin $(OUTPUT_DIR)/$$(private_product)/; fi
 	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/HLN2_NonTLoader_eMMC_DDR.bin ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/HLN2_NonTLoader_eMMC_DDR.bin $(OUTPUT_DIR)/$$(private_product)/; fi
 	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/Software_Downloader_Helan2.zip ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/Software_Downloader_Helan2.zip $(OUTPUT_DIR)/; fi
 	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/modules ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/modules; fi &&\
@@ -167,7 +167,7 @@ build_droid_$$(product):
 ##!!## first time publish: all for two
 PUBLISHING_FILES2+=Software_Downloader.zip:./:m:md5
 PUBLISHING_FILES2+=Software_Downloader_Helan2.zip:./:o:md5
-PUBLISHING_FILES+=$$(product)/LoopSelf.bin:o:md5
+PUBLISHING_FILES+=$$(product)/WTM.bin:o:md5
 PUBLISHING_FILES+=$$(product)/HLN2_NonTLoader_eMMC_DDR.bin:o:md5
 PUBLISHING_FILES+=$$(product)/obm.bin:o:md5
 PUBLISHING_FILES+=$$(product)/obm_trusted_tz.bin:o:md5
