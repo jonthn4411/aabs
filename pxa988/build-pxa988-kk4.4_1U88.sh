08fd5abbfd8bbcee7fc28390945395e24a30b1aa
Jia Guo <guojia@marvell.com>
2014-05-28 10:16:21 +0800

    pxa988: separate pxa1u88 build
    Change-Id: Ie433eaaad8a9acab10b25655d4bea7d3c71f0cfa
    Signed-off-by: Jia Guo <guojia@marvell.com>

---
+++ pxa988/build-pxa988-kk4.4_1U88.sh
@@ -0,0 +1,11 @@
+export ABS_SOC=pxa988
+export ABS_DROID_BRANCH=kk4.4
+export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+if [ -z "ABS_BUILDHOST_DEF" ]; then
+    ABS_BUILDHOST_DEF=buildhost.def
+fi
+export ABS_BUILDHOST_DEF
+
+export ABS_UNIQUE_MANIFEST_BRANCH=1
+export ABS_PUBLISH_DIR_BASE=/autobuild/android/pxa1U88
+export ABS_BUILD_DEVICES="pxa1U88dkb_def:pxa1U88dkb"
