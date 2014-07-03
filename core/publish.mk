f21cd684273a01569181b564776e52b24db72047
Jia Guo <guojia@marvell.com>
2014-07-03 16:35:44 +0800

    Revert "update XA988 FF devices and make link for FF dailybuild"
    This reverts commit 7dead082fed967ff432e9ccb8a1c653e90bd5e4b.
    
    Change-Id: I150632cb8f104ee15ebf6941f687dfcaa54ca127

--- core/publish.mk
+++ core/publish.mk
@@ -2,7 +2,6 @@
 $(call check-variables, PUBLISH_DIR)
 
 MD5_FILE:=checksums.md5
-ABS_PUBLISH_DIR_FF_BASE:=/autobuild/android/pxa1L88FF
 define cp-with-md5
 	@echo "publishing mandatory file:$(1) to $(2)"
 	@mkdir -p $(dir $(2))
@@ -93,7 +92,6 @@ publish_dir:
 	$(hide)if [ ! -d "$(PUBLISH_DIR)" ]; then \
 	    mkdir -p $(PUBLISH_DIR) && chmod g+w $(PUBLISH_DIR); \
 	fi
-	@ln -s $(ABS_PUBLISH_DIR_FF_BASE) $(PUBLISH_DIR)
 
 clean_md5_file:
 	@echo -n > $(OUTPUT_DIR)/$(MD5_FILE)
