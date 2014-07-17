e8ba3f40c5bbe485ffdcee3c80d038b62657d135
Fuqiang Zhai <fuqzhai@marvell.com>
2014-07-17 18:31:40 +0800

    remove the no use code
    Signed-off-by: Fuqiang Zhai <fuqzhai@marvell.com>
    Change-Id: Ia1b076202897a5471d4d13e5a6237237ea8cca59
    Reviewed-on: http://shgit.marvell.com/r/113277

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -20,88 +20,9 @@ MAKE_EXT4FS := out/host/linux-x86/bin/make_ext4fs
 MKBOOTFS := out/host/linux-x86/bin/mkbootfs
 MINIGZIP := out/host/linux-x86/bin/minigzip
 
-define define-clean-droid-kernel-target
-tw:=$$(subst :,  , $(1) )
-product:=$$(word 1, $$(tw) )
-device:=$$(word 2, $$(tw) )
-.PHONY:clean_droid_kernel_$$(product)
-clean_droid_kernel_$$(product): clean_droid_$$(product) clean_kernel_$$(product)
-
-.PHONY:clean_droid_$$(product)
-clean_droid_$$(product): private_product:=$$(product)
-clean_droid_$$(product): private_device:=$$(device)
-clean_droid_$$(product):
-	$(log) "clean android ..."
-	$(hide)cd $(SRC_DIR) && \
-	source ./build/envsetup.sh &&
-	chooseproduct $(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
-	make clean
-	$(log) "    done"
-
-.PHONY:clean_kernel_$$(product)
-clean_kernel_$$(product): private_product:=$$(product)
-clean_kernel_$$(product): private_device:=$$(device)
-clean_kernel_$$(product):
-	$(log) "clean kernel ..."
-	$(hide)cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && make clean
-	$(log) "    done"
-endef
-
-#we need first build the android, so we get the root dir 
 # and then we build the kernel images with the root dir and get the package of corresponding modules
 # and then we use those module package to build corresponding android package.
 
-define define-build-droid-kernel-target
-tw:=$$(subst :,  , $(1) )
-product:=$$(word 1, $$(tw) )
-device:=$$(word 2, $$(tw) )
-.PHONY:build_droid_kernel_$$(product)
-build_droid_kernel_$$(product): build_droid_$$(product) build_droid_otapackage_$$(product) 
-endef
-
-MAKE_JOBS := 8
-export KERNEL_TOOLCHAIN_PREFIX
-export MAKE_JOBS
-
-#$1:kernel_config
-#$2:build variant
-define define-build-kernel-target
-tw:=$$(subst :,  , $(1) )
-product:=$$(word 1, $$(tw) )
-device:=$$(word 2, $$(tw) )
-.PHONY: build_kernel_$$(product)
-
-#make sure that PUBLISHING_FILES_XXX is a simply expanded variable
-PUBLISHING_FILES+=$$(product)/uImage:m:md5
-build_kernel_$$(product): private_product:=$$(product)
-build_kernel_$$(product): private_device:=$$(device)
-build_kernel_$$(product): output_dir
-	$(log) "[$$(private_product)]starting to build kernel ..."
-	$(hide)cd $(SRC_DIR) && \
-	source ./build/envsetup.sh && \
-	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
-	cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && \
-	make kernel
-	$(log) "[$$(private_product)]starting to build modules ..."
-	$(hide)cd $(SRC_DIR) && \
-	source ./build/envsetup.sh && \
-	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant $(DROID_VARIANT) && \
-	cd $(SRC_DIR)/$(KERNELSRC_TOPDIR) && \
-	make modules
-	$(hide)mkdir -p $(OUTPUT_DIR)/$$(private_product)
-	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/kernel/uImage $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/kernel/vmlinux $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)cp $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/kernel/System.map $(OUTPUT_DIR)/$$(private_product)/
-	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/modules ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/modules; fi &&\
-	mkdir -p $(OUTPUT_DIR)/$$(private_product)/modules
-	$(hide)cp -af $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/kernel/modules  $(OUTPUT_DIR)/$$(private_product)/
-
-	$(hide)if [ -d $(OUTPUT_DIR)/$$(private_product)/dtb ]; then rm -fr $(OUTPUT_DIR)/$$(private_product)/dtb; fi &&\
-	mkdir -p $(OUTPUT_DIR)/$$(private_product)/dtb
-	$(hide)cp -af $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/*.dtb  $(OUTPUT_DIR)/$$(private_product)/dtb/
-
-	$(log) "  done."
-endef
 
 ##!!## build rootfs for android, make -j4 android, copy root, copy ramdisk/userdata/system.img to outdir XXX
 #$1:build variant
@@ -351,36 +272,4 @@ endef
 #	$(log) "[$$(private_product)] no android OTA package build ..."
 #endef
 
-define define-build-droid-tool
-tw:=$$(subst :,  , $(1) )
-product:=$$(word 1, $$(tw) )
-device:=$$(word 2, $$(tw) )
-.PHONY: build_droid_tool_$$(product)
-build_droid_tool_$$(product): private_product:=$$(product)
-build_droid_tool_$$(product): private_device:=$$(device)
-build_droid_tool_$$(product): build_droid_$$(product)
-	$(log) "[$$(private_product)] rebuilding android source code with eng for tools ..."
-	$(hide)cd $(SRC_DIR) && \
-	source ./build/envsetup.sh && \
-	chooseproduct $$(private_product) && choosetype $(DROID_TYPE) && choosevariant eng && \
-	make -j8
-	$(hide)if [ -d $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools ]; then rm -fr $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools; fi
-	$(hide)echo "  copy and make tools image ..."
-	$(hide)mkdir -p $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools/bin
-	$(hide)cd $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/symbols/system && \
-	cp -af $(TOOLS_LIST) $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools/bin
-	$(hide)$(SRC_DIR)/$(MAKE_EXT4FS) -s -l 65536k -b 1024 -L tool $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools.img $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools
-	$(hide)cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/tools.img $(OUTPUT_DIR)/$$(private_product)/
-	tar zcvf $(OUTPUT_DIR)/$$(private_product)/tools.tgz -C $(SRC_DIR)/$(DROID_OUT)/$$(private_device) tools
 
-PUBLISHING_FILES+=$$(product)/tools.img:o:md5
-PUBLISHING_FILES+=$$(product)/tools.tgz:o:md5
-
-endef
-
-$(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call define-build-droid-kernel-target,$(bv)) )\
-				$(eval $(call define-build-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-target,$(bv)) ) \
-				$(eval $(call define-clean-droid-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-otapackage,$(bv)) ) \
-)
