ea197c83e3b389e199cecf47e8685aaf89aacf5a
Jianchun Zuo <jzuo@marvell.com>
2014-08-22 11:05:25 +0800

    pxa1928: remove typo errro
    Change-Id: I806946377f668e1e8ae5580842c169175295b9ad
    Signed-off-by: Jianchun Zuo <jzuo@marvell.com>

--- pxa1928/board.mk
+++ pxa1928/board.mk
@@ -4,7 +4,7 @@ else
 ifneq ($(filter $(abs_droid_branch),pdk5.0),)
 ABS_BUILD_DEVICES?=pxa1928dkb_tz:pxa1928dkb pxa1928ff_tz:pxa1928ff
 else
-ifneq ($(filter $(abs_droid_branch),pdk5.0_generic),))
+ifneq ($(filter $(abs_droid_branch),pdk5.0_generic),)
 ABS_BUILD_DEVICES?=mini_arm64:arm64
 else
 ABS_BUILD_DEVICES?=pxa1928dkb_tz:pxa1928dkb pxa1928ff_tz:pxa1928ff pxa1928dkb_dsds:pxa1928dkb
