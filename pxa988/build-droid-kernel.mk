9bbed586c3d300e46ca5fe0bec17c1654b0b349e
Wei Liang <liangwei@marvell.com>
2014-08-29 10:07:02 +0800

    pxa988: add cache.img support
    Change-Id: I72a2e2a634d27434fba3c4f401b8d21e3c41115b
    Signed-off-by: Wei Liang <liangwei@marvell.com>

--- pxa988/build-droid-kernel.mk
+++ pxa988/build-droid-kernel.mk
@@ -137,6 +137,7 @@ build_droid_$$(product):
 	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/primary_gpt_8g ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/primary_gpt_8g $(OUTPUT_DIR)/$$(private_product)/; fi
 	$(hide)if [ -e $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/secondary_gpt_8g ]; then cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/secondary_gpt_8g $(OUTPUT_DIR)/$$(private_product)/; fi
 	$(hide)cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/system.img $(OUTPUT_DIR)/$$(private_product)
+	$(hide)cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/cache.img $(OUTPUT_DIR)/$$(private_product)
 	$(hide)cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/system/build.prop $(OUTPUT_DIR)/$$(private_product)
 	$(hide)if [ -d $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/telephony/ ]; then \
 	cp -p -r $(SRC_DIR)/$(DROID_OUT)/$$(private_device)/telephony/* $(OUTPUT_DIR)/$$(private_product)/; fi
@@ -206,6 +207,7 @@ PUBLISHING_FILES+=$$(product)/userdata_8g.img:o:md5
 PUBLISHING_FILES+=$$(product)/primary_gpt_8g:o:md5
 PUBLISHING_FILES+=$$(product)/secondary_gpt_8g:o:md5
 PUBLISHING_FILES+=$$(product)/system.img:m:md5
+PUBLISHING_FILES+=$$(product)/cache.img:m:md5
 PUBLISHING_FILES+=$$(product)/ramdisk.img:m:md5
 PUBLISHING_FILES+=$$(product)/ramdisk-rooted.img:o:md5
 PUBLISHING_FILES2+=$$(product)/symbols_system.tgz:./$$(product)/symbols/:o:md5
