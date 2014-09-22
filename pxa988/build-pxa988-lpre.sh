8762161dbdb44d1aff3ddbcad8f2c346f5b491f5
Jia Guo <guojia@marvell.com>
2014-09-22 13:57:52 +0800

    pxa988:include buildhost-lpre.def for lpre
    Change-Id: I5b3826f6ad479b47e86a64d093e2ff35d68584a7
    Signed-off-by: Jia Guo <guojia@marvell.com>

--- pxa988/build-pxa988-lpre.sh
+++ pxa988/build-pxa988-lpre.sh
@@ -1,9 +1,7 @@
 export ABS_SOC=pxa988
 export ABS_DROID_BRANCH=lpre
 #export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
-if [ -z "ABS_BUILDHOST_DEF" ]; then
-    ABS_BUILDHOST_DEF=buildhost-lpre.def
-fi
+export ABS_BUILDHOST_DEF=buildhost-lpre.def
 export ABS_BUILDHOST_DEF
 
 export ABS_UNIQUE_MANIFEST_BRANCH=1
