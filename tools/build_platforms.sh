2abe50f3f5a183ddd7efe8f9d9ab7fbcde0c3d42
Fuqiang Zhai <fuqzhai@marvell.com>
2014-10-21 12:34:28 +0800

    add build kernel image
    Signed-off-by: Fuqiang Zhai <fuqzhai@marvell.com>
    Change-Id: I2c32baf8e578bd84586dbd9fde61d0f4158b55cd
    Reviewed-on: http://shgit.marvell.com/r/126506

--- tools/build_platforms.sh
+++ tools/build_platforms.sh
@@ -149,9 +149,7 @@ if [ -x ${ABS_TOP_DIR}/${soc}/build-${platform}.sh ]; then
         TARGET_PKGSRC="pkgsrc"
         TARGET_EMAIL="email"
     fi
-    if [ "$ABS_FORCE_BUILD" = "true" ]; then
         FORCE_BUILD="force"
-    fi
 	if [ "$dryrun_flag" == true ]; then
 		echo "[aabs]will-run:./core/autobuild.sh clobber source pkgsrc publish autotest email $rlsname" | tee -a $LOG
 	else
