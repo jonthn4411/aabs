8631973a90db253c02c386c59cec582aaf68e3a6
Fuqiang Zhai <fuqzhai@marvell.com>
2014-11-05 19:55:00 +0800

    update pxa1920 build host info
    Signed-off-by: Fuqiang Zhai <fuqzhai@marvell.com>
    Change-Id: I82410cb63310464ed0e872ede5e3c94dc144291f
    Reviewed-on: http://shgit.marvell.com/r/129178

--- pxa1920/build-pxa1920-lp5.0.sh
+++ pxa1920/build-pxa1920-lp5.0.sh
@@ -1,7 +1,7 @@
 export ABS_SOC=pxa1920
 export ABS_DROID_BRANCH=lp5.0
 #export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
-export ABS_BUILDHOST_DEF=buildhost-lpre.def
+export ABS_BUILDHOST_DEF=buildhost.def
 export ABS_BUILDHOST_DEF
 
 export ABS_UNIQUE_MANIFEST_BRANCH=1
