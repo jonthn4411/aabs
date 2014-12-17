4ab16702c42faa4fca4a489ccae24dc00f5a24e5
Moshe Nissim <mnissim@marvell.com>
2014-12-17 22:01:59 +0300

    Publish: LWG_ZIP_RF.bin rf
        remove Skylark_LWG_B0_CP6X_ZIP.bin from files
    
        will use the build output file of LWG_ZIP_RF.bin instead
    
    Change-Id: I02958275ab78c39d86ad32814d5f80862a0e0add
    Signed-off-by: Moshe Nissim <mnissim@marvell.com>
    Reviewed-on: http://shgit.marvell.com/r/140926

--- pxa1928/build-droid-kernel.mk
+++ pxa1928/build-droid-kernel.mk
@@ -115,7 +115,7 @@ PUBLISHING_FILES2+=$$(product)/HL_LWG_DKB_B0_CP6X_MDB.bin:./$$(product)/flash/:o
 PUBLISHING_FILES2+=$$(product)/Skylark_LWG_B0_CN_CP6X.bin:./$$(product)/flash/:o:md5
 PUBLISHING_FILES2+=$$(product)/Skylark_LWG_B0_EU_CP6X.bin:./$$(product)/flash/:o:md5
 PUBLISHING_FILES2+=$$(product)/Skylark_LWG_B0_NA_CP6X.bin:./$$(product)/flash/:o:md5
-PUBLISHING_FILES2+=$$(product)/Skylark_LWG_B0_CP6X_ZIP.bin:./$$(product)/flash/:o:md5
+PUBLISHING_FILES2+=$$(product)/LWG_ZIP_RF.bin:./$$(product)/flash/:o:md5
 PUBLISHING_FILES2+=$$(product)/HL_LWG_DKB_B0_CP6X_MDB.txt:./$$(product)/debug/:o:md5
 PUBLISHING_FILES2+=$$(product)/CP6X_version.txt:./$$(product)/debug/:o:md5
 endif
