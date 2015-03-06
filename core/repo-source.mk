1d05bac783a496f0eb72dfa246fda27900ea40c6
Steve Yang <chaoyang@marvell.com>
2015-03-06 01:03:13 -0500

    AABA: publish package to the same place by devices
    Change-Id: I54a2c1ecb27f6a502fff57262e81069e1e1ba32d

--- core/repo-source.mk
+++ core/repo-source.mk
@@ -17,11 +17,17 @@ source: output_dir
 		repo init -u ssh://$(GIT_MANIFEST) -b $(ABS_MANIFEST_BRANCH) --repo-url ssh://$(GIT_REPO) --reference $(GIT_LOCAL_MIRROR); \
 	fi && \
 	if [ -n "$(ABS_MANIFEST_FILE)" ]; then \
+		cp ${ABS_BUILD_MANIFEST} .repo/manifests/; \
 		repo init -m $(ABS_MANIFEST_FILE); \
 	fi && \
 	repo sync
 	$(log) "saving manifest file..."
-	$(hide)cd $(SRC_DIR) && repo manifest -r -o $(OUTPUT_DIR)/manifest.xml
+	$(hide)if [ "$(FLAG_DISTRIBUTED_BUILD)" = "true" ]; then \
+		cp $(ABS_BUILD_MANIFEST) $(OUTPUT_DIR)/manifest.xml; \
+	else \
+		cd $(SRC_DIR); \
+		repo manifest -r -o $(OUTPUT_DIR)/manifest.xml; \
+	fi
 #if an expection happened, repo doesn't exit with a non-zero value, we use below command to make sure the manifest.xml is generated.
 	$(hide)ls $(OUTPUT_DIR)/manifest.xml > /dev/null
 	$(hide)git rev-parse HEAD >$(OUTPUT_DIR)/abs.commit
