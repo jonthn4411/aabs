973af3aa67a5e3ee2a0633030e5f4e60455cbad1
Wenzhao Fang <wzhfang@marvell.com>
2015-03-23 16:10:13 +0800

    add 1956dkb  device on lp5.1 branch
    Change-Id: Iaed9ec94523ca859a79ae39bf585cebe93dba410
    Signed-off-by: Wenzhao Fang <wzhfang@marvell.com>

---
+++ pxa1956/board.mk
@@ -0,0 +1,52 @@
+ABS_BUILD_DEVICES ?=pxa1936dkb_tz:pxa1936dkb pxa1936ff_tz:pxa1936ff pxa1936ff_64bit:pxa1936ff pxa1936dkb_64bit:pxa1936dkb
+BOARD:=pxa1936
+ANDROID_VERSION:=$(ABS_DROID_BRANCH)
+PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
+
+#
+# Include goal for build android and kernels.
+#
+include $(ABS_SOC)/build-droid-kernel.mk
+#include $(ABS_SOC)/build-droid-kernel-test.mk
+
+# Include goal for build UBoot and OBM
+#include $(ABS_SOC)/build-uboot-obm.mk
+
+# Include goal for build software downloader
+#include $(ABS_SOC)/build-swd.mk
+
+# Include goal for build wtpsp
+#include $(ABS_SOC)/build-wtpsp.mk
+
+#define the combined goal to include all build goals
+build: build_device
+#build: build_swd
+
+.PHONY:build_device
+define define-build-device
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+
+build_device_$$(product): private_product:=$$(product)
+build_device_$$(product): private_device:=$$(device)
+build_device_$$(product): build_droid_kernel_$$(product)
+build_device: build_device_$$(product)
+endef
+
+$(foreach bv1, $(ABS_BUILD_DEVICES), $(eval $(call define-build-device,$(bv1) ) ))
+
+.PHONY:clean
+clean:clean_device clean_swd clean_wtpsp
+
+define define-clean-device
+tw:=$$(subst :,  , $(1) )
+product:=$$(word 1, $$(tw) )
+device:=$$(word 2, $$(tw) )
+clean_device_$$(product): private_product:=$$(product)
+clean_device_$$(product): private_device:=$$(device)
+clean_device_$$(product): clean_uboot_obm_$$(product) clean_droid_kernel_$$(product)
+clean_device:clean_device_$$(product)
+endef
+$(foreach bv, $(ABS_BUILD_DEVICES), $(eval $(call define-clean-device,$(bv) ) ) )
+
