17381d66af69f2628e65270b932df2519d1b05ed
wanglee <wanglee@marvell.com>
2015-04-08 14:10:59 +0800

    PXA1928:Open OTA build
    Change-Id: I8893cbd35b8135f4ea48a1ffd99ddf28c80a9255
    Signed-off-by: wanglee <wanglee@marvell.com>

--- pxa1928/build-droid-kernel.mk
+++ pxa1928/build-droid-kernel.mk
@@ -188,7 +188,7 @@ build_droid_otapackage_$$(product)_$$(kernel_cfg)_$$(boot_cfg): private_device:=
 build_droid_otapackage_$$(product)_$$(kernel_cfg)_$$(boot_cfg): private_kcfg:=$$(kernel_cfg)
 build_droid_otapackage_$$(product)_$$(kernel_cfg)_$$(boot_cfg): private_bcfg:=$$(boot_cfg)
 build_droid_otapackage_$$(product)_$$(kernel_cfg)_$$(boot_cfg): output_dir
-ifneq ($(filter $(ABS_DROID_BRANCH),aosp pdk5.0 lmr1 lmr1_32 lp5.1),)
+ifneq ($(filter $(ABS_DROID_BRANCH),aosp pdk5.0 lmr1 lmr1_32),)
 	$$(log) "disalbe otapackage build by generating fake ota files temporally"
 	$$(hide)touch $$(OUTPUT_DIR)/$$(private_product)/$$(private_product)-ota-mrvl.zip
 	$$(hide)touch $$(OUTPUT_DIR)/$$(private_product)/$$(private_product)-ota-mrvl-recovery.zip
