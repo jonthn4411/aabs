72dbc48877f85656d77e3b58cdcce961de240126
Jia Guo <guojia@marvell.com>
2015-07-21 15:44:03 +0800

    pxa1936:add pxa1936 mpre build
    Change-Id: I39d400248595c800ccc61e34a8a39bd1384fcb5a
    Signed-off-by: Jia Guo <guojia@marvell.com>

---
+++ pxa1936/build-pxa1936-mpre.sh
@@ -0,0 +1,9 @@
+export ABS_SOC=pxa1936
+export ABS_DROID_BRANCH=mpre
+#export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export ABS_BUILDHOST_DEF=buildhost-lpre.def
+export ABS_BUILDHOST_DEF
+
+export ABS_UNIQUE_MANIFEST_BRANCH=1
+export ABS_BUILD_DEVICES="pxa1908dkb_tz:pxa1908dkb"
+
