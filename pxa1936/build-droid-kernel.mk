ab58bb805a37f41f52606fb1af1f98c95da892d7
cjzang <cjzang@marvell.com>
2015-08-14 22:03:41 +0800

    modify the script.
    Change-Id: Ie2a366a387b8d165cf2097f66d76dfda220ec72f
    Signed-off-by: cjzang <cjzang@marvell.com>

--- pxa1936/build-droid-kernel.mk
+++ pxa1936/build-droid-kernel.mk
@@ -572,9 +572,5 @@ endef
 #				$(eval $(call define-build-droid-debug-img,$(bv)) ) \
 #)
 
-$(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call define-build-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-target,$(bv)) ) \
-				$(eval $(call define-clean-droid-kernel-target,$(bv)) ) \
-				$(eval $(call define-build-droid-otapackage,$(bv)) ) \
-				$(eval $(call define-build-debug-kernel-target,$(bv)) ) \
+$(foreach bv,$(ABS_BUILD_DEVICES), $(eval $(call $(eval $(call define-build-droid-target,$(bv)) ) \
 )
