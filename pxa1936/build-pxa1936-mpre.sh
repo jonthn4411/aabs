e91b290e01e0ad81291a04ab8f2f41ab7cd066b0
Wenzhao Fang <wzhfang@marvell.com>
2015-08-20 13:16:28 +0800

    pxa1928: add pxa1928_64bit device into dailybuild
    Change-Id: Id4239348b4cddb19d4b3254e40dc497049aa66d7
    Signed-off-by: Wenzhao Fang <wzhfang@marvell.com>

--- pxa1936/build-pxa1936-mpre.sh
+++ pxa1936/build-pxa1936-mpre.sh
@@ -5,4 +5,4 @@ export ABS_BUILDHOST_DEF=buildhost-lpre.def
 export ABS_BUILDHOST_DEF
 
 export ABS_UNIQUE_MANIFEST_BRANCH=1
-export ABS_BUILD_DEVICES=${ABS_BUILD_DEVICES:-"pxa1928ff_tz:pxa1928ff pxa1908dkb_tz:pxa1908dkb pxa1936dkb_tz:pxa1936dkb pxa1936tk_tz:pxa1936tk pxa1908sl_tz:pxa1908sl"}
+export ABS_BUILD_DEVICES=${ABS_BUILD_DEVICES:-"pxa1928ff_64bit:pxa1928ff pxa1928ff_tz:pxa1928ff pxa1908dkb_tz:pxa1908dkb pxa1936dkb_tz:pxa1936dkb pxa1936tk_tz:pxa1936tk pxa1908sl_tz:pxa1908sl"}
