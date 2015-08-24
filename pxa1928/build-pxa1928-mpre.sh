0d60f7f0952a7cf5cb1327a2cc7008e2c8b7a203
mazhiy <mazhiy@marvell.com>
2015-08-24 16:43:20 +0800

    Add 1928 mpre IR7 into daily build
    Signed-off-by: mazhiy <mazhiy@marvell.com>

---
+++ pxa1928/build-pxa1928-mpre.sh
@@ -0,0 +1,8 @@
+export ABS_SOC=pxa1928
+export ABS_DROID_BRANCH=mpre
+#export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
+export ABS_BUILDHOST_DEF=buildhost-lpre.def
+export ABS_BUILDHOST_DEF
+
+export ABS_UNIQUE_MANIFEST_BRANCH=1
+export ABS_BUILD_DEVICES=${ABS_BUILD_DEVICES:-"pxa1928ff_64bit:pxa1928ff pxa1928ff_tz:pxa1928ff"}
