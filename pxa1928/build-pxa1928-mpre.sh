b1d12a362f5fd871036e3896c1d57983aba9b2db
mazhiy <mazhiy@marvell.com>
2015-08-25 11:11:30 +0800

    Remove 64bit from complie
    Signed-off-by: mazhiy <mazhiy@marvell.com>

--- pxa1928/build-pxa1928-mpre.sh
+++ pxa1928/build-pxa1928-mpre.sh
@@ -5,4 +5,4 @@ export ABS_BUILDHOST_DEF=buildhost-lpre.def
 export ABS_BUILDHOST_DEF
 
 export ABS_UNIQUE_MANIFEST_BRANCH=1
-export ABS_BUILD_DEVICES=${ABS_BUILD_DEVICES:-"pxa1928ff_64bit:pxa1928ff pxa1928ff_tz:pxa1928ff"}
+export ABS_BUILD_DEVICES=${ABS_BUILD_DEVICES:-"pxa1928ff_tz:pxa1928ff"}
