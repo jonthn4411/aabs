e0610725bb0c3d815ee60c01ca43bab26d0d0148
Chunjie Zang <cjzang@marvell.com>
2015-09-10 15:28:07 +0800

    aabs: update scripts
    1. modify the ABS_DROID_BRANCH var to add name.
    
    Change-Id: I7dc06a09bc55b31d256b23bcbf00a09d82982a13
    Signed-off-by: Chunjie Zang <cjzang@marvell.com>

--- pxa1908/build-pxa1908-add.sh
+++ pxa1908/build-pxa1908-add.sh
@@ -1,5 +1,5 @@
 export ABS_SOC=pxa1908
-export ABS_DROID_BRANCH=lp5.1
+export ABS_DROID_BRANCH=add
 export PATH=/usr/lib/jvm/java-6-sun/bin/:$PATH
 if [ -z "ABS_BUILDHOST_DEF" ]; then
     ABS_BUILDHOST_DEF=buildhost.def
