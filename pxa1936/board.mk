71beb9fa446bd9434ee3487f4fc45f2c4ac7aebd
Fuqiang Zhai <fuqzhai@marvell.com>
2015-09-11 11:06:47 +0800

    pxa1936:remove pxa1936ff and pxa1908ff device
    Change-Id: Iad6cf8b42f6497baaac18210d0e9c4b7c67443c7
    Signed-off-by: Fuqiang Zhai <fuqzhai@marvell.com>

--- pxa1936/board.mk
+++ pxa1936/board.mk
@@ -1,4 +1,4 @@
-ABS_BUILD_DEVICES ?=pxa1936ff_tz:pxa1936ff pxa1908dkb_tz:pxa1908dkb pxa1908FF_tz:pxa1908FF pxa1936dkb_64bit:pxa1936dkb pxa1936dkb_tz:pxa1936dkb pxa1956dkb_tz:pxa1956dkb pxa1936tk_tz:pxa1936tk pxa1908sl_tz:pxa1908sl
+ABS_BUILD_DEVICES ?=pxa1908dkb_tz:pxa1908dkb pxa1936dkb_64bit:pxa1936dkb pxa1936dkb_tz:pxa1936dkb pxa1956dkb_tz:pxa1956dkb pxa1936tk_tz:pxa1936tk pxa1908sl_tz:pxa1908sl
 BOARD:=pxa1936
 ANDROID_VERSION:=$(ABS_DROID_BRANCH)
 PRODUCT_CODE:=$(BOARD)-$(ANDROID_VERSION)
